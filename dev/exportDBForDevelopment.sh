622d99672d762f8cb24fb5b91b8214c77376e94e
Peter Xu <px.peter.xu@gmail.com>
2019-02-10 19:20:18 -0500

    Fix an issue with the database exports

--- dev/exportDBForDevelopment.sh
+++ dev/exportDBForDevelopment.sh
@@ -7,11 +7,10 @@
 
 OUTPUT_FILE="$1"
 FIRST_SEASON="$2"
-FIRST_COURSE_SEASON="$3"
 
-if [ -z "$OUTPUT_FILE" ] || [ -z "$FIRST_SEASON" ] || [ -z "$FIRST_COURSE_SEASON" ]; then
-  echo 'Usage: bash exportDBForDevelopment.sh outputFile.sql 201701 201801'
-  echo '       bash exportDBForDevelopment.sh (output) (eval start season) (courses start season)'
+if [ -z "$OUTPUT_FILE" ] || [ -z "$FIRST_SEASON" ]; then
+  echo 'Usage: bash exportDBForDevelopment.sh outputFile.sql 201701'
+  echo '       bash exportDBForDevelopment.sh (output) (first season to get data)'
   echo ''
 
   echo 'Exports a version of production tables with somewhat fudged data for'
@@ -28,10 +27,8 @@ echo 'USE yale_advanced_oci;' >> "$OUTPUT_FILE"
 # Data tables for most tables in yale_advanced_oci
 mysqldump yale_advanced_oci \
   barnes_noble_sections \
-  evaluation_courses \
-  evaluation_course_names \
-  evaluation_questions \
   exam_groups \
+  evaluation_questions \
   key_value_store \
   oci_seasons \
   textbooks \
@@ -41,17 +38,25 @@ mysqldump yale_advanced_oci \
   textbook_prices >> "$OUTPUT_FILE"
 
 # Dump a subset of certain course tables
-mysqldump --lock-all-tables --where="(SELECT season FROM course_names cn WHERE cn.course_id = courses.id LIMIT 1) >= $FIRST_COURSE_SEASON" yale_advanced_oci courses >> "$OUTPUT_FILE"
-mysqldump --lock-all-tables --where="season >= $FIRST_COURSE_SEASON" yale_advanced_oci course_names >> "$OUTPUT_FILE"
+mysqldump --lock-all-tables --where="(SELECT season FROM course_names cn WHERE cn.course_id = courses.id LIMIT 1) >= $FIRST_SEASON" yale_advanced_oci courses >> "$OUTPUT_FILE"
+mysqldump --lock-all-tables --where="season >= $FIRST_SEASON" yale_advanced_oci course_names >> "$OUTPUT_FILE"
 
 TABLES="course_areas course_flags course_professors course_sessions course_skills"
 for table in $TABLES; do
-  mysqldump --lock-all-tables --where="(SELECT season FROM course_names cn WHERE cn.course_id = $table.course_id LIMIT 1) >= $FIRST_COURSE_SEASON" yale_advanced_oci "$table" >> "$OUTPUT_FILE"
+  mysqldump --lock-all-tables --where="(SELECT season FROM course_names cn WHERE cn.course_id = $table.course_id LIMIT 1) >= $FIRST_SEASON" yale_advanced_oci "$table" >> "$OUTPUT_FILE"
 done
 
-# Dump a sbuset of certain evaluation tables
+# Dump a subset of certain evaluation tables
+# SELECT * FROM evaluation_courses WHERE (SELECT season FROM course_names cn WHERE cn.course_id = courses.id LIMIT 1) >= 201803
+mysqldump --lock-all-tables --where="season >= $FIRST_SEASON" yale_advanced_oci evaluation_courses >> "$OUTPUT_FILE"
+mysqldump --lock-all-tables --where="season >= $FIRST_SEASON" yale_advanced_oci evaluation_course_names >> "$OUTPUT_FILE"
+
+EVALUATION_TABLES="evaluation_ratings"
+for table in $EVALUATION_TABLES; do
+  mysqldump --lock-all-tables --where="(SELECT season FROM evaluation_course_names cn WHERE cn.course_id = $table.course_id LIMIT 1) >= $FIRST_SEASON" yale_advanced_oci "$table" >> "$OUTPUT_FILE"
+done
+
 mysqldump --lock-all-tables --where="id % 5 = 1 AND (SELECT season FROM evaluation_courses ec WHERE ec.id = evaluation_comments.course_id) >= $FIRST_SEASON" yale_advanced_oci evaluation_comments >> "$OUTPUT_FILE"
-mysqldump --lock-all-tables --where="(SELECT season FROM evaluation_courses ec WHERE ec.id = evaluation_ratings.course_id) >= $FIRST_SEASON" yale_advanced_oci evaluation_ratings >> "$OUTPUT_FILE"
 
 # Dump only structure for student-tied and generated tables
 mysqldump -d yale_advanced_oci course_json worksheet_courses >> "$OUTPUT_FILE"
