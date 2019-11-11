fd4f0303e6aa887cff1fe2585fec83c960e3bfa1
Murad Avliyakulov <murad.avliyakulov@yale.edu>
2019-11-11 18:16:56 -0500

    Coding out the calendar generation

--- web/GenerateIcs.php
+++ web/GenerateIcs.php
@@ -6,9 +6,16 @@ function renderEventFromData()
     
 }
 
-function renderIcsFile()
+function renderIcsFile($ociIds, $ociIdData, $season, $outputStream)
 {
-    
+    $calendar = new \Eluceo\iCal\Component\Calendar('https://coursetable.com');
+    $event = new \Eluceo\iCal\Component\Event();
+    $vEvent->setDtStart(new \DateTime('2012-12-24'));
+    $vEvent->setDtEnd(new \DateTime('2012-12-24'));
+    $vEvent->setNoTime(true);
+    $vEvent->setSummary('Christmas');
+    $calendar->addComponent($event);
+    $calendar->render();
 }
 
 ini_set('memory_limit', '256M');
