f916cbbe5e73dc1977b3c43fa0cb5136e0a29e23
Murad Avliyakulov <murad.avliyakulov@yale.edu>
2019-11-18 16:51:12 -0500

    Test calendar gen with hard coded event

--- web/GenerateIcs.php
+++ web/GenerateIcs.php
@@ -1,21 +1,26 @@
 <?php
 require_once 'includes/ProjectCommon.php';
 
-function renderEventFromData()
+function addEventToCalendar()
 {
     
 }
 
-function renderIcsFile($ociIds, $ociIdData, $season, $outputStream)
+function addImportantEvents()
+{
+
+}
+
+function createIcsFile($ociIds, $ociIdData, $outputStream)
 {
     $calendar = new \Eluceo\iCal\Component\Calendar('https://coursetable.com');
     $event = new \Eluceo\iCal\Component\Event();
-    $vEvent->setDtStart(new \DateTime('2012-12-24'));
-    $vEvent->setDtEnd(new \DateTime('2012-12-24'));
-    $vEvent->setNoTime(true);
-    $vEvent->setSummary('Christmas');
+    $event->setDtStart(new \DateTime('2019-11-18'));
+    $event->setDtEnd(new \DateTime('2019-11-18'));
+    $event->setNoTime(true);
+    $event->setSummary('Christmas');
     $calendar->addComponent($event);
-    $calendar->render();
+    echo $calendar->render();
 }
 
 ini_set('memory_limit', '256M');
