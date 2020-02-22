f5c2cb7e8fa42c6bdc75e40d86b953c271b3e5bc
Harshal Sheth <hsheth2@gmail.com>
2020-02-22 16:51:41 -0500

    Update is_development header

--- web/includes/Config.php
+++ web/includes/Config.php
@@ -1,2 +1,8 @@
 <?php
-define('IS_DEVELOPMENT', true);
+# Credentials may specify some configuration, which should
+# take precedence over the config defined here.
+require_once __DIR__ . '/Credentials.php';
+
+if (!defined('IS_DEVELOPMENT')) {
+    define('IS_DEVELOPMENT', true);
+}
