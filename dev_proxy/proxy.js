ce07f10e6154c86904a1fb008106d4e8b45a16d7
Harshal Sheth <hsheth2@gmail.com>
2020-06-23 00:53:22 -0400

    Setup new frontend

---
+++ dev_proxy/proxy.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const fs = require('fs')
+const https = require('https')
+
+const app = express();
+const port = 8080;
+
+app.use('/legacy_api', createProxyMiddleware({
+  target: 'http://nginx:8080/',
+  pathRewrite: {
+    '^/legacy_api': '/',  // remove base path
+  },
+}));
+
+app.use('/', createProxyMiddleware({
+  target: 'http://frontend:3000',
+}));
+
+// Serve with SSL.
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+  .listen(port, () => {
+    console.log(`Dev proxy listening on port ${port}`)
+  });
