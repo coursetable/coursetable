1c4eac7f22499ba37682d2df4c9f3cd81553f90e
Harshal Sheth <hsheth2@gmail.com>
2020-07-03 21:27:39 -0400

    Avoid full window refresh

--- frontend/src/components/WorksheetToggleButton.js
+++ frontend/src/components/WorksheetToggleButton.js
@@ -4,10 +4,11 @@ import common_styles from '../styles/common.module.css';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
 import { useUser } from '../user';
+import { toast } from 'react-toastify';
 
-const App = props => {
+const WorksheetToggleButton = (props) => {
   var [inWorksheet, setInWorksheet] = useState(false);
-  const { user } = useUser();
+  const { user, userRefresh } = useUser();
   if (user.worksheet === undefined) return <div>Signin</div>;
   if (!inWorksheet) {
     for (let i = 0; i < user.worksheet.length; i++) {
@@ -25,11 +26,9 @@ const App = props => {
     inWorksheet ? (add_remove = 'remove') : (add_remove = 'add');
     axios
       .get(
-        `/legacy_api/WorksheetActions.php?action=${add_remove}&season=${
-          props.season_code
-        }&ociId=${props.crn}`
+        `/legacy_api/WorksheetActions.php?action=${add_remove}&season=${props.season_code}&ociId=${props.crn}`
       )
-      .then(response => {
+      .then((response) => {
         console.log(response.data);
       });
   }
@@ -39,7 +38,10 @@ const App = props => {
     add_remove_course();
     setInWorksheet(!inWorksheet);
     console.log('toggle ', props.crn + ' ' + props.season_code);
-    window.location.reload();
+    userRefresh().catch((err) => {
+      toast.error('Failed to update worksheet');
+      console.error(err);
+    });
   }
   return (
     <Button
@@ -51,4 +53,4 @@ const App = props => {
   );
 };
 
-export default App;
+export default WorksheetToggleButton;
