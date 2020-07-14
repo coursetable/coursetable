9d2980251872451e9279adbf1c034809f9e97210
Kevin Hu <kevinhuwest@gmail.com>
2020-07-13 22:16:34 -0400

    Sticky field headers in search results

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -9,6 +9,8 @@ import Styles from './SearchResults.module.css';
 
 import { Accordion, Card, Col, Row } from 'react-bootstrap';
 
+import Sticky from 'react-sticky-el';
+
 const App = ({ data }) => {
 	const { width } = useWindowDimensions();
 
@@ -17,21 +19,26 @@ const App = ({ data }) => {
 	return (
 		<Accordion className={Styles.results_container}>
 			{!isMobile && (
-				<div className={'m-0 pl-2 pr-3 ' + Styles.results_header}>
-					<Card key={-1} className={'pl-3 pr-3 ' + Styles.results_header_card}>
-						<Card.Header className={Styles.results_header_inner}>
-							<Row className={'px-0 justify-content-between'}>
-								<Col md={4}>Description</Col>
-								<Col md={2}>Rating</Col>
-								<Col md={2}>Workload</Col>
-								<Col md={2}>Areas</Col>
-								<Col md={2}></Col>
-							</Row>
-						</Card.Header>
-					</Card>
-				</div>
+				<Sticky>
+					<div className={'m-0 ' + Styles.results_header}>
+						<Card
+							key={-1}
+							className={'pl-3 pr-3 shadow-sm ' + Styles.results_header_card}
+						>
+							<Card.Header className={Styles.results_header_inner}>
+								<Row className={'px-0 justify-content-between'}>
+									<Col md={4}>Description</Col>
+									<Col md={2}>Rating</Col>
+									<Col md={2}>Workload</Col>
+									<Col md={2}>Areas</Col>
+									<Col md={2}></Col>
+								</Row>
+							</Card.Header>
+						</Card>
+					</div>
+				</Sticky>
 			)}
-			<div style={{paddingTop:'2rem'}}>
+			<div>
 				{data.map(course => (
 					<SearchResultsItem course={flatten(course)} isMobile={isMobile} />
 				))}
