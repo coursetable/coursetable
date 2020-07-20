8dd51b3b7dab0345cbb8a396a0808093df3657d3
Kevin Hu <kevinhuwest@gmail.com>
2020-07-20 14:54:58 -0400

    Removed accordion in search results

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -7,7 +7,7 @@ import { useWindowDimensions } from './WindowDimensionsProvider';
 
 import Styles from './SearchResults.module.css';
 
-import { Accordion, Card, Col, Row } from 'react-bootstrap';
+import { Container, Card, Col, Row } from 'react-bootstrap';
 
 import Sticky from 'react-sticky-el';
 
@@ -17,33 +17,24 @@ const App = ({ data }) => {
 	const isMobile = width < 768;
 
 	return (
-		<Accordion className={`shadow-sm ${Styles.results_container}`}>
+		<Container className={`shadow-sm ${Styles.results_container}`}>
 			{!isMobile && (
 				<Sticky>
-					<div className={'m-0 ' + Styles.results_header}>
-						<Card
-							key={-1}
-							className={'pl-3 pr-3 shadow-sm ' + Styles.results_header_card}
-						>
-							<Card.Header className={Styles.results_header_inner}>
-								<Row className={'px-0 justify-content-between'}>
-									<Col md={4}>Description</Col>
-									<Col md={2}>Rating</Col>
-									<Col md={2}>Workload</Col>
-									<Col md={2}>Areas</Col>
-									<Col md={2}></Col>
-								</Row>
-							</Card.Header>
-						</Card>
-					</div>
+					<Row className={`px-0 py-2 shadow-sm justify-content-between ${Styles.results_header_row}`}>
+						<Col md={4}>Description</Col>
+						<Col md={2}>Rating</Col>
+						<Col md={2}>Workload</Col>
+						<Col md={2}>Areas</Col>
+						<Col md={2}></Col>
+					</Row>
 				</Sticky>
 			)}
-			<div>
+			<div className="px-2">
 				{data.map(course => (
 					<SearchResultsItem course={flatten(course)} isMobile={isMobile} />
 				))}
 			</div>
-		</Accordion>
+		</Container>
 	);
 };
 
