7a0c0c6f30af00878a728f28dba23f72c7cb0d50
Juliana Viola <juliana.viola@yale.edu>
2020-07-22 20:54:06 -0400

    Still trying to get login working

--- frontend/src/components/FBLoginButton.js
+++ frontend/src/components/FBLoginButton.js
@@ -1,14 +1,96 @@
-// Modified from: https://www.djamware.com/post/5e6d6a9a05efef95f94c4aed/reactjs-tutorial-facebook-login-example
-
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import { Button } from 'react-bootstrap';
-// import FB from '../fb';
+// import '../fb';
 // import styles from './FBLoginButton.module.css';
 
-function FBLoginButton(props) {
+// const FB = window.FB;
 
-  function handleClick() {
+// A lot of code from https://github.com/Jerryg6j3/react-fbsdk-example
+class FBLoginButton extends Component {
+
+  componentDidMount() {
+	  window.fbAsyncInit = function() {
+	    window.FB.init({
+        appId: '185745958145518',
+        channelUrl: '/legacy_api/FacebookChannel.php', // Channel File for x-domain communication
+        status: true,
+	      cookie: true,  // enable cookies to allow the server to access the session
+	      xfbml: true,  // parse social plugins on this page
+	      version: 'v2.1',
+	    });
+	
+	    // Now that we've initialized the JavaScript SDK, we call
+	    // FB.getLoginStatus().  This function gets the state of the
+	    // person visiting this page and can return one of three states to
+	    // the callback you provide.  They can be:
+	    //
+	    // 1. Logged into your app ('connected')
+	    // 2. Logged into Facebook, but not your app ('not_authorized')
+	    // 3. Not logged into Facebook and can't tell if they are logged into
+	    //    your app or not.
+	    //
+	    // These three cases are handled in the callback function.
+	    window.FB.getLoginStatus(function(response) {
+	      this.statusChangeCallback(response);
+	    }.bind(this));
+	  }.bind(this);
+    
+	  // Load the SDK asynchronously
+	  (function(d, s, id) {
+	    var js, fjs = d.getElementsByTagName(s)[0];
+	    if (d.getElementById(id)) return;
+	    js = d.createElement(s); js.id = id;
+	    js.src = "//connect.facebook.net/en_US/sdk.js";
+	    fjs.parentNode.insertBefore(js, fjs);
+	  }(document, 'script', 'facebook-jssdk'));
+	}
+
+	// Here we run a very simple test of the Graph API after login is
+	// successful.  See statusChangeCallback() for when this call is made.
+	testAPI() {
+	  console.log('Welcome!  Fetching your information.... ');
+	  window.FB.api('/me', function(response) {
+	  console.log('Successful login for: ' + response.name);
+	  // document.getElementById('status').innerHTML =
+	  //   'Thanks for logging in, ' + response.name + '!';
+	  });
+	}
+	
+	// This is called with the results from from FB.getLoginStatus().
+	statusChangeCallback(response) {
+	  console.log('statusChangeCallback');
+	  console.log(response);
+	  // The response object is returned with a status field that lets the
+	  // app know the current login status of the person.
+	  // Full docs on the response object can be found in the documentation
+	  // for FB.getLoginStatus().
+	  if (response.status === 'connected') {
+	    // Logged into your app and Facebook.
+	    this.testAPI();
+	  } else if (response.status === 'not_authorized') {
+	    // The person is logged into Facebook, but not your app.
+	    document.getElementById('status').innerHTML = 'Please log ' +
+	      'into this app.';
+	  } else {
+	    // The person is not logged into Facebook, so we're not sure if
+	    // they are logged into this app or not.
+	    document.getElementById('status').innerHTML = 'Please log ' +
+	    'into Facebook.';
+	  }
+	}
+	
+	// This function is called when someone finishes with the Login
+	// Button.  See the onlogin handler attached to it in the sample
+	// code below.
+	checkLoginState() {
+	  window.FB.getLoginStatus(function(response) {
+	    this.statusChangeCallback(response);
+	  }.bind(this));
+	}
+
+  handleClick() {
     console.log('Clicking FBLoginButton.');
+    window.FB.login(this.checkLoginState());
   }
 
 //   /**
@@ -127,24 +209,26 @@ function FBLoginButton(props) {
 //     );
 //   };
 
-  return (
-    <div>
-      {/* See https://developers.facebook.com/apps/185745958145518/fb-login/quickstart/ */}
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-      {props.isLoggedIn && (
-        <Button 
-          class="btn friend-worksheets-btn"
-          // href="/legacy_api/index.php?forcelogin=1"
-          onClick={handleClick}
-          // style="display: inline-block;"
-        >
-          {/* TODO: Style FB icon so it's not so ugly lol */}
-          <i className="fa fa-facebook"></i>
-          <span>See friends' worksheets</span>
-        </Button>
-      )}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        {/* See https://developers.facebook.com/apps/185745958145518/fb-login/quickstart/ */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        {this.props.isLoggedIn && (
+          <Button 
+            className="btn friend-worksheets-btn"
+            // href="/legacy_api/index.php?forcelogin=1"
+            onClick={this.handleClick}
+            // style="display: inline-block;"
+          >
+            {/* TODO: Style FB icon so it's not so ugly lol */}
+            <i className="fa fa-facebook"></i>
+            <span>See friends' worksheets</span>
+          </Button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default FBLoginButton;
