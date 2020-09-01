62fcb585919c8b73dda6e7fb5f86cf9b22a960ab
Max Yuan <maxyuan6717@gmail.com>
2020-08-31 23:26:25 -0400

    Clean up worksheetlist.js and fix overflow-scroll bug

--- frontend/src/components/WorksheetList.js
+++ frontend/src/components/WorksheetList.js
@@ -6,28 +6,12 @@ import WorksheetHideButton from './WorksheetHideButton';
 import WorksheetRowDropdown from './WorksheetRowDropdown';
 
 export default class WorksheetList extends React.Component {
-  setSeason = (season_code) => {
-    this.props.onSeasonChange(season_code);
-  };
-
   showModal = (listing) => {
     this.props.showModal(listing);
   };
 
   toggleCourse = (season_code, crn, hidden) => {
     this.props.toggleCourse(season_code, crn, hidden);
-  };
-
-  parseListings = (listings, season_codes) => {
-    let parsed_courses = {};
-    season_codes.forEach((season_code) => {
-      parsed_courses[season_code] = [];
-    });
-    listings.forEach((listing) => {
-      if (parsed_courses[listing['season_code']] === undefined) return;
-      parsed_courses[listing['season_code']].push(listing);
-    });
-    return parsed_courses;
   };
 
   isHidden = (season_code, crn) => {
@@ -38,74 +22,62 @@ export default class WorksheetList extends React.Component {
     return false;
   };
 
-  buildHtml = (season_codes, parsed_courses) => {
+  buildHtml = (cur_season, courses) => {
     let items = [];
-    season_codes.sort();
-    season_codes.reverse();
     let id = 0;
-    season_codes.forEach((season) => {
-      parsed_courses[season].forEach((course) => {
-        items.push(
-          <ListGroup.Item
-            key={id++}
-            className={styles.clickable + ' py-1 px-2'}
-            onMouseEnter={() => this.props.setHoverCourse(course)}
-            onMouseLeave={() => this.props.setHoverCourse(null)}
-          >
-            <div className={styles.bookmark}>
-              <WorksheetToggleButton
-                alwaysRed={true}
-                crn={course.crn}
-                season_code={season}
-                modal={false}
-                hasSeason={this.props.hasSeason}
-              />
-            </div>
-            <Row className="align-items-center mx-auto">
-              <Col xs="auto" className="pl-0 pr-2 my-auto">
-                <Row className="m-auto">
-                  <WorksheetHideButton
-                    toggleCourse={this.props.toggleCourse}
-                    crn={course.crn}
-                    season_code={season}
-                  />
-                </Row>
-              </Col>
-              <Col
-                className={
-                  (this.isHidden(season, course.crn)
-                    ? styles.hidden + ' '
-                    : '') +
-                  styles.list_text +
-                  ' px-0'
-                }
-                onClick={() => this.showModal(course)}
-              >
-                <strong>{course['course_code']}</strong>
-                <br />
-                <span className={styles.course_title}>
-                  {course['course.title']}
-                </span>
-              </Col>
-            </Row>
-          </ListGroup.Item>
-        );
-      });
+    courses.forEach((course) => {
+      items.push(
+        <ListGroup.Item
+          key={id++}
+          className={styles.clickable + ' py-1 px-2'}
+          onMouseEnter={() => this.props.setHoverCourse(course)}
+          onMouseLeave={() => this.props.setHoverCourse(null)}
+        >
+          <div className={styles.bookmark}>
+            <WorksheetToggleButton
+              alwaysRed={true}
+              crn={course.crn}
+              season_code={cur_season}
+              modal={false}
+              hasSeason={this.props.hasSeason}
+            />
+          </div>
+          <Row className="align-items-center mx-auto">
+            <Col xs="auto" className="pl-0 pr-2 my-auto">
+              <Row className="m-auto">
+                <WorksheetHideButton
+                  toggleCourse={this.props.toggleCourse}
+                  crn={course.crn}
+                  season_code={cur_season}
+                />
+              </Row>
+            </Col>
+            <Col
+              className={
+                (this.isHidden(cur_season, course.crn)
+                  ? styles.hidden + ' '
+                  : '') +
+                styles.list_text +
+                ' px-0'
+              }
+              onClick={() => this.showModal(course)}
+            >
+              <strong>{course['course_code']}</strong>
+              <br />
+              <span className={styles.course_title}>
+                {course['course.title']}
+              </span>
+            </Col>
+          </Row>
+        </ListGroup.Item>
+      );
     });
+
     return items;
   };
 
   render() {
-    const parsed_courses = this.parseListings(
-      this.props.courses,
-      this.props.season_codes
-    );
-    const items = this.buildHtml(
-      this.props.season_codes,
-      parsed_courses,
-      this.props.cur_season
-    );
-
+    const items = this.buildHtml(this.props.cur_season, this.props.courses);
     return (
       <div className={styles.container}>
         <WorksheetRowDropdown
@@ -115,9 +87,9 @@ export default class WorksheetList extends React.Component {
           setFbPerson={this.props.setFbPerson}
           cur_person={this.props.cur_person}
         />
-        <ListGroup variant="flush" className={styles.table}>
-          {items}
-        </ListGroup>
+        <div className={styles.table}>
+          <ListGroup variant="flush">{items}</ListGroup>
+        </div>
       </div>
     );
   }
