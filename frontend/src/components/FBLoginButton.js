d6662f4a6f83791b38d829cb9377e53a1c49c6ce
Harshal Sheth <hsheth2@gmail.com>
2020-10-03 02:59:57 -0400

    Add TODOs for FB

--- frontend/src/components/FBLoginButton.js
+++ frontend/src/components/FBLoginButton.js
@@ -13,6 +13,8 @@ function FBLoginButton() {
 
   // Note: window.FB setup via index.html.
 
+  // TODO: add support for refreshing facebook auth status
+
   const syncFacebook = useCallback(() => {
     return axios.get('/legacy_api/FetchFacebookData.php').then(({ data }) => {
       if (!data.success) {
@@ -62,6 +64,7 @@ function FBLoginButton() {
   }, [syncFacebook]);
 
   const handleLogoutClick = useCallback(() => {
+    // TODO: disconnect_facebook does not implement it correctly.
     axios
       .get('/legacy_api/Table.php?disconnect_facebook')
       .then(() => {
