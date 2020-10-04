f3b0655935bb593d8a649da48481f51004b18e2c
Kevin Hu <kevinhuwest@gmail.com>
2020-10-04 18:55:38 -0400

    Require authentication for challenge API

--- challenge/challenge.js
+++ challenge/challenge.js
@@ -24,24 +24,35 @@ import { constructChallenge, verifyChallenge, decrypt } from './utils.js';
  * @prop res - return object
  */
 app.get('/challenge/request', (req, res) => {
-  // randomize the selected challenge courses by
-  // randomly choosing a minimum rating
-  const minRating = 1 + Math.random() * 4;
+	// get authentication headers
+	const netid = req.header('x-coursetable-netid'); // user's NetID
+	const authd = req.header('x-coursetable-authd'); // if user is logged in
 
-  query({
-    query: requestEvalsQuery,
-    endpoint: GRAPHQL_ENDPOINT,
-    variables: {
-      season: CHALLENGE_SEASON,
-      minRating: minRating,
-    },
-  })
-    .then((response) => {
-      res.json({ body: constructChallenge(response) });
-    })
-    .catch((error) => {
-      res.json({ body: error });
-    });
+	// require NetID authentication
+	if (authd !== 'true') {
+		res.status(400).send({
+			error: 'NOT_AUTHENTICATED',
+		});
+	}
+
+	// randomize the selected challenge courses by
+	// randomly choosing a minimum rating
+	const minRating = 1 + Math.random() * 4;
+
+	query({
+		query: requestEvalsQuery,
+		endpoint: GRAPHQL_ENDPOINT,
+		variables: {
+			season: CHALLENGE_SEASON,
+			minRating: minRating,
+		},
+	})
+		.then(response => {
+			res.json({ body: constructChallenge(response) });
+		})
+		.catch(error => {
+			res.json({ body: error });
+		});
 });
 
 /**
@@ -50,57 +61,74 @@ app.get('/challenge/request', (req, res) => {
  * @prop res - return object
  */
 app.post('/challenge/verify', (req, res) => {
-  let { token, salt, answers } = req.body;
+	// get authentication headers
+	const netid = req.header('x-coursetable-netid'); // user's NetID
+	const authd = req.header('x-coursetable-authd'); // if user is logged in
 
-  let secrets; // the decrypted token
+	// require NetID authentication
+	if (authd !== 'true') {
+		res.status(400).send({
+			error: 'NOT_AUTHENTICATED',
+		});
+	}
 
-  let secretRatingIds; // for retrieving the correct ones from the database
+	let { token, salt, answers } = req.body;
 
-  // list in the format "<question_id>_<rating_index>" to verify
-  // the submitted answers match those encoded in the token
-  let secretRatings;
-  let answerRatings;
+	let secrets; // the decrypted token
 
-  // catch malformed token decryption errors
-  try {
-    secrets = JSON.parse(decrypt(token, salt));
-    secretRatingIds = secrets.map((x) => x['courseRatingId']);
-    secretRatings = secrets.map(
-      (x) => `${x['courseRatingId']}_${x['courseRatingIndex']}`
-    );
-  } catch (e) {
-    res.json({ body: 'INVALID_TOKEN' });
-  }
+	let secretRatingIds; // for retrieving the correct ones from the database
 
-  // catch malformed answer JSON errors
-  try {
-    answerRatings = answers.map(
-      (x) => `${x['courseRatingId']}_${x['courseRatingIndex']}`
-    );
-  } catch (e) {
-    res.json({ body: 'INVALID_ANSWERS' });
-  }
+	// list in the format "<question_id>_<rating_index>" to verify
+	// the submitted answers match those encoded in the token
+	let secretRatings;
+	let answerRatings;
 
-  // make sure the provided ratings IDs and indices match those we have
-  if (secretRatings.sort().join(',') !== answerRatings.sort().join(',')) {
-    res.json({ body: 'INVALID_TOKEN' });
-  }
+	// catch malformed token decryption errors
+	try {
+		secrets = JSON.parse(decrypt(token, salt));
+		secretRatingIds = secrets.map(x => x['courseRatingId']);
+		secretRatings = secrets.map(
+			x => `${x['courseRatingId']}_${x['courseRatingIndex']}`
+		);
+	} catch (e) {
+		res.status(400).send({
+			error: 'INVALID_TOKEN',
+		});
+	}
 
-  query({
-    query: verifyEvalsQuery,
-    endpoint: GRAPHQL_ENDPOINT,
-    variables: {
-      questionIds: secretRatingIds,
-    },
-  })
-    .then((response) => {
-      res.json({ body: verifyChallenge(response, answers) });
-    })
-    .catch((error) => {
-      res.json({ body: error });
-    });
+	// catch malformed answer JSON errors
+	try {
+		answerRatings = answers.map(
+			x => `${x['courseRatingId']}_${x['courseRatingIndex']}`
+		);
+	} catch (e) {
+		res.status(400).send({
+			error: 'MALFORMED_ANSWERS',
+		});
+	}
+
+	// make sure the provided ratings IDs and indices match those we have
+	if (secretRatings.sort().join(',') !== answerRatings.sort().join(',')) {
+		res.status(400).send({
+			error: 'INVALID_TOKEN',
+		});
+	}
+
+	query({
+		query: verifyEvalsQuery,
+		endpoint: GRAPHQL_ENDPOINT,
+		variables: {
+			questionIds: secretRatingIds,
+		},
+	})
+		.then(response => {
+			res.json({ body: verifyChallenge(response, answers) });
+		})
+		.catch(error => {
+			res.json({ body: error });
+		});
 });
 
 app.listen(PORT, () => {
-  console.log(`Challenge API listening at http://localhost:${PORT}`);
+	console.log(`Challenge API listening at http://localhost:${PORT}`);
 });
