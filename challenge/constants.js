5d04ddfc92d3fa4b8064e6778c09e8e8982638b2
Kevin Hu <kevinhuwest@gmail.com>
2020-10-04 13:57:35 -0400

    Expand valid question codes for challenge

--- challenge/constants.js
+++ challenge/constants.js
@@ -3,9 +3,22 @@ export const PORT = 4096;
 
 export const CHALLENGE_ALGORITHM = 'aes-256-ctr';
 export const CHALLENGE_PASSWORD =
-  process.env.CHALLENGE_PASSWORD || 'thisisapassword';
+	process.env.CHALLENGE_PASSWORD || 'thisisapassword';
 
 export const NUM_CHALLENGE_COURSES = 3; // number of courses to select for the challenge
 export const CHALLENGE_SEASON = '201903'; // season to select the challenge from
 
-export const VALID_QUESTION_CODES = ['YC006', 'YC306', 'YC404']; // question codes to be used
+// question codes to be used
+// currently question codes with a 'rating' tag
+// https://github.com/coursetable/ferry/blob/master/resources/question_tags.csv
+export const VALID_QUESTION_CODES = [
+	'MG008',
+	'YC006',
+	'GS008',
+	'YC404',
+	'YC306',
+	'GS404',
+	'PH002',
+	'FS1009',
+	'SU104',
+];
