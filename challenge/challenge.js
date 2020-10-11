c3fed88977f08571de9c069d98a7efbe46fd153a
Kevin Hu <kevinhuwest@gmail.com>
2020-10-11 15:10:53 -0400

    Prettier challenge scripts

--- challenge/challenge.js
+++ challenge/challenge.js
@@ -13,10 +13,10 @@ const { query } = graphqurl;
 app.use(morgan('tiny'));
 
 import {
-	PORT,
-	GRAPHQL_ENDPOINT,
-	CHALLENGE_SEASON,
-	MAX_CHALLENGE_REQUESTS,
+  PORT,
+  GRAPHQL_ENDPOINT,
+  CHALLENGE_SEASON,
+  MAX_CHALLENGE_REQUESTS,
 } from './constants.js';
 
 import { requestEvalsQuery, verifyEvalsQuery } from './queries.js';
@@ -25,16 +25,16 @@ import { constructChallenge, verifyChallenge, decrypt } from './utils.js';
 
 import mysql from 'mysql';
 var mysqlConnection = mysql.createConnection({
-	host: 'mysql',
-	user: 'root',
-	password: 'GoCourseTable',
-	database: 'yaleplus',
+  host: 'mysql',
+  user: 'root',
+  password: 'GoCourseTable',
+  database: 'yaleplus',
 });
 
 // open the MySQL connection
 mysqlConnection.connect(error => {
-	if (error) throw error;
-	console.log('Successfully connected to the database.');
+  if (error) throw error;
+  console.log('Successfully connected to the database.');
 });
 
 /**
@@ -43,97 +43,97 @@ mysqlConnection.connect(error => {
  * @prop res - return object
  */
 app.get('/challenge/request', (req, res) => {
-	// get authentication headers
-	const netid = req.header('x-coursetable-netid'); // user's NetID
-	const authd = req.header('x-coursetable-authd'); // if user is logged in
+  // get authentication headers
+  const netid = req.header('x-coursetable-netid'); // user's NetID
+  const authd = req.header('x-coursetable-authd'); // if user is logged in
 
-	// require NetID authentication
-	if (authd !== 'true') {
-		res.status(400).send({
-			error: 'NOT_AUTHENTICATED',
-		});
-		return;
-	}
+  // require NetID authentication
+  if (authd !== 'true') {
+    res.status(400).send({
+      error: 'NOT_AUTHENTICATED',
+    });
+    return;
+  }
 
-	// use mysql.escape() to prevent injection attacks
-	mysqlConnection.query(
-		`SELECT challengeTries,evaluationsEnabled FROM StudentBluebookSettings WHERE netid=${mysql.escape(
-			netid
-		)} LIMIT 1`,
-		(error, results, fields) => {
-			// catch general query errors
-			if (error) {
-				console.error(error.message);
-				res.status(400).send({
-					error: 'INTERNAL_ERROR',
-				});
-				return;
-			}
+  // use mysql.escape() to prevent injection attacks
+  mysqlConnection.query(
+    `SELECT challengeTries,evaluationsEnabled FROM StudentBluebookSettings WHERE netid=${mysql.escape(
+      netid
+    )} LIMIT 1`,
+    (error, results, fields) => {
+      // catch general query errors
+      if (error) {
+        console.error(error.message);
+        res.status(400).send({
+          error: 'INTERNAL_ERROR',
+        });
+        return;
+      }
 
-			// affirm single user retrieved
-			if (results.length !== 1) {
-				res.status(400).send({
-					error: 'USER_NOT_FOUND',
-				});
-				return;
-			}
+      // affirm single user retrieved
+      if (results.length !== 1) {
+        res.status(400).send({
+          error: 'USER_NOT_FOUND',
+        });
+        return;
+      }
 
-			const challengeTries = results[0]['challengeTries'];
-			const evaluationsEnabled = results[0]['evaluationsEnabled'];
+      const challengeTries = results[0]['challengeTries'];
+      const evaluationsEnabled = results[0]['evaluationsEnabled'];
 
-			if (evaluationsEnabled === 1) {
-				res.status(400).send({
-					error: 'ALREADY_ENABLED',
-				});
-				return;
-			}
-			// limit number of challenge requests
-			if (challengeTries >= MAX_CHALLENGE_REQUESTS) {
-				res.status(400).send({
-					error: 'MAX_TRIES_REACHED',
-				});
-				return;
-			}
+      if (evaluationsEnabled === 1) {
+        res.status(400).send({
+          error: 'ALREADY_ENABLED',
+        });
+        return;
+      }
+      // limit number of challenge requests
+      if (challengeTries >= MAX_CHALLENGE_REQUESTS) {
+        res.status(400).send({
+          error: 'MAX_TRIES_REACHED',
+        });
+        return;
+      }
 
-			// update number of tries
-			mysqlConnection.query(
-				`UPDATE StudentBluebookSettings SET challengeTries=${challengeTries +
-					1} WHERE netid=${mysql.escape(netid)}`,
-				(error, results, fields) => {
-					// catch general query errors
-					if (error) {
-						console.error(error.message);
-						res.status(400).send({
-							error: 'INTERNAL_ERROR',
-						});
-						return;
-					}
+      // update number of tries
+      mysqlConnection.query(
+        `UPDATE StudentBluebookSettings SET challengeTries=${challengeTries +
+          1} WHERE netid=${mysql.escape(netid)}`,
+        (error, results, fields) => {
+          // catch general query errors
+          if (error) {
+            console.error(error.message);
+            res.status(400).send({
+              error: 'INTERNAL_ERROR',
+            });
+            return;
+          }
 
-					// randomize the selected challenge courses by
-					// randomly choosing a minimum rating
-					const minRating = 1 + Math.random() * 4;
+          // randomize the selected challenge courses by
+          // randomly choosing a minimum rating
+          const minRating = 1 + Math.random() * 4;
 
-					query({
-						query: requestEvalsQuery,
-						endpoint: GRAPHQL_ENDPOINT,
-						variables: {
-							season: CHALLENGE_SEASON,
-							minRating: minRating,
-						},
-					})
-						.then(response => {
-							res.json({
-								body: constructChallenge(response),
-								challengeTries: challengeTries + 1,
-							});
-						})
-						.catch(error => {
-							res.json({ body: error, challengeTries: challengeTries + 1 });
-						});
-				}
-			);
-		}
-	);
+          query({
+            query: requestEvalsQuery,
+            endpoint: GRAPHQL_ENDPOINT,
+            variables: {
+              season: CHALLENGE_SEASON,
+              minRating: minRating,
+            },
+          })
+            .then(response => {
+              res.json({
+                body: constructChallenge(response),
+                challengeTries: challengeTries + 1,
+              });
+            })
+            .catch(error => {
+              res.json({ body: error, challengeTries: challengeTries + 1 });
+            });
+        }
+      );
+    }
+  );
 });
 
 /**
@@ -142,170 +142,170 @@ app.get('/challenge/request', (req, res) => {
  * @prop res - return object
  */
 app.post('/challenge/verify', (req, res) => {
-	// get authentication headers
-	const netid = req.header('x-coursetable-netid'); // user's NetID
-	const authd = req.header('x-coursetable-authd'); // if user is logged in
+  // get authentication headers
+  const netid = req.header('x-coursetable-netid'); // user's NetID
+  const authd = req.header('x-coursetable-authd'); // if user is logged in
 
-	// require NetID authentication
-	if (authd !== 'true') {
-		res.status(400).send({
-			error: 'NOT_AUTHENTICATED',
-		});
-	}
+  // require NetID authentication
+  if (authd !== 'true') {
+    res.status(400).send({
+      error: 'NOT_AUTHENTICATED',
+    });
+  }
 
-	mysqlConnection.query(
-		`SELECT challengeTries,evaluationsEnabled FROM StudentBluebookSettings WHERE netid=${mysql.escape(
-			netid
-		)} LIMIT 1`,
-		(error, results, fields) => {
-			// catch general query errors
-			if (error) {
-				console.error(error.message);
-				res.status(400).send({
-					error: 'INTERNAL_ERROR',
-				});
-				return;
-			}
+  mysqlConnection.query(
+    `SELECT challengeTries,evaluationsEnabled FROM StudentBluebookSettings WHERE netid=${mysql.escape(
+      netid
+    )} LIMIT 1`,
+    (error, results, fields) => {
+      // catch general query errors
+      if (error) {
+        console.error(error.message);
+        res.status(400).send({
+          error: 'INTERNAL_ERROR',
+        });
+        return;
+      }
 
-			// affirm single user retrieved
-			if (results.length !== 1) {
-				res.status(400).send({
-					error: 'USER_NOT_FOUND',
-				});
-				return;
-			}
+      // affirm single user retrieved
+      if (results.length !== 1) {
+        res.status(400).send({
+          error: 'USER_NOT_FOUND',
+        });
+        return;
+      }
 
-			const challengeTries = results[0]['challengeTries'];
-			const evaluationsEnabled = results[0]['evaluationsEnabled'];
+      const challengeTries = results[0]['challengeTries'];
+      const evaluationsEnabled = results[0]['evaluationsEnabled'];
 
-			if (evaluationsEnabled === 1) {
-				res.status(400).send({
-					error: 'ALREADY_ENABLED',
-				});
-				return;
-			}
+      if (evaluationsEnabled === 1) {
+        res.status(400).send({
+          error: 'ALREADY_ENABLED',
+        });
+        return;
+      }
 
-			// limit number of challenge requests
-			if (challengeTries >= MAX_CHALLENGE_REQUESTS) {
-				res.status(400).send({
-					error: 'MAX_TRIES_REACHED',
-				});
-				return;
-			}
+      // limit number of challenge requests
+      if (challengeTries >= MAX_CHALLENGE_REQUESTS) {
+        res.status(400).send({
+          error: 'MAX_TRIES_REACHED',
+        });
+        return;
+      }
 
-			// update number of tries
-			mysqlConnection.query(
-				`UPDATE StudentBluebookSettings SET challengeTries=${challengeTries +
-					1} WHERE netid=${mysql.escape(netid)}`,
-				(error, results, fields) => {
-					// catch general query errors
-					if (error) {
-						console.error(error.message);
-						res.status(400).send({
-							error: 'INTERNAL_ERROR',
-						});
-						return;
-					}
+      // update number of tries
+      mysqlConnection.query(
+        `UPDATE StudentBluebookSettings SET challengeTries=${challengeTries +
+          1} WHERE netid=${mysql.escape(netid)}`,
+        (error, results, fields) => {
+          // catch general query errors
+          if (error) {
+            console.error(error.message);
+            res.status(400).send({
+              error: 'INTERNAL_ERROR',
+            });
+            return;
+          }
 
-					let { token, salt, answers } = req.body;
+          let { token, salt, answers } = req.body;
 
-					let secrets; // the decrypted token
+          let secrets; // the decrypted token
 
-					let secretRatingIds; // for retrieving the correct ones from the database
+          let secretRatingIds; // for retrieving the correct ones from the database
 
-					// list in the format "<question_id>_<rating_index>" to verify
-					// the submitted answers match those encoded in the token
-					let secretRatings;
-					let answerRatings;
+          // list in the format "<question_id>_<rating_index>" to verify
+          // the submitted answers match those encoded in the token
+          let secretRatings;
+          let answerRatings;
 
-					// catch malformed token decryption errors
-					try {
-						secrets = JSON.parse(decrypt(token, salt));
-						secretRatingIds = secrets.map(x => x['courseRatingId']);
-						secretRatings = secrets.map(
-							x => `${x['courseRatingId']}_${x['courseRatingIndex']}`
-						);
-					} catch (e) {
-						res.status(400).send({
-							error: 'INVALID_TOKEN',
-							challengeTries: challengeTries + 1,
-						});
-						return;
-					}
+          // catch malformed token decryption errors
+          try {
+            secrets = JSON.parse(decrypt(token, salt));
+            secretRatingIds = secrets.map(x => x['courseRatingId']);
+            secretRatings = secrets.map(
+              x => `${x['courseRatingId']}_${x['courseRatingIndex']}`
+            );
+          } catch (e) {
+            res.status(400).send({
+              error: 'INVALID_TOKEN',
+              challengeTries: challengeTries + 1,
+            });
+            return;
+          }
 
-					// catch malformed answer JSON errors
-					try {
-						answers = JSON.parse(answers);
-						answerRatings = answers.map(
-							x => `${x['courseRatingId']}_${x['courseRatingIndex']}`
-						);
-					} catch (e) {
-						console.error(e);
-						res.status(400).send({
-							error: 'MALFORMED_ANSWERS',
-							challengeTries: challengeTries + 1,
-						});
-						return;
-					}
+          // catch malformed answer JSON errors
+          try {
+            answers = JSON.parse(answers);
+            answerRatings = answers.map(
+              x => `${x['courseRatingId']}_${x['courseRatingIndex']}`
+            );
+          } catch (e) {
+            console.error(e);
+            res.status(400).send({
+              error: 'MALFORMED_ANSWERS',
+              challengeTries: challengeTries + 1,
+            });
+            return;
+          }
 
-					// make sure the provided ratings IDs and indices match those we have
-					if (
-						secretRatings.sort().join(',') !== answerRatings.sort().join(',')
-					) {
-						res.status(400).send({
-							error: 'INVALID_TOKEN',
-							challengeTries: challengeTries + 1,
-						});
-						return;
-					}
+          // make sure the provided ratings IDs and indices match those we have
+          if (
+            secretRatings.sort().join(',') !== answerRatings.sort().join(',')
+          ) {
+            res.status(400).send({
+              error: 'INVALID_TOKEN',
+              challengeTries: challengeTries + 1,
+            });
+            return;
+          }
 
-					query({
-						query: verifyEvalsQuery,
-						endpoint: GRAPHQL_ENDPOINT,
-						variables: {
-							questionIds: secretRatingIds,
-						},
-					})
-						.then(response => {
-							// if answers are incorrect, respond with error
-							if (!verifyChallenge(response, answers)) {
-								res.status(400).send({
-									error: 'INCORRECT',
-									challengeTries: challengeTries + 1,
-								});
-							}
+          query({
+            query: verifyEvalsQuery,
+            endpoint: GRAPHQL_ENDPOINT,
+            variables: {
+              questionIds: secretRatingIds,
+            },
+          })
+            .then(response => {
+              // if answers are incorrect, respond with error
+              if (!verifyChallenge(response, answers)) {
+                res.status(400).send({
+                  error: 'INCORRECT',
+                  challengeTries: challengeTries + 1,
+                });
+              }
 
-							// otherwise, enable evaluations
-							mysqlConnection.query(
-								`UPDATE StudentBluebookSettings SET evaluationsEnabled=1 WHERE netid=${mysql.escape(
-									netid
-								)}`,
-								(error, results, fields) => {
-									// catch general query errors
-									if (error) {
-										console.error(error.message);
-										res.status(400).send({
-											error: 'INTERNAL_ERROR',
-										});
-										return;
-									}
+              // otherwise, enable evaluations
+              mysqlConnection.query(
+                `UPDATE StudentBluebookSettings SET evaluationsEnabled=1 WHERE netid=${mysql.escape(
+                  netid
+                )}`,
+                (error, results, fields) => {
+                  // catch general query errors
+                  if (error) {
+                    console.error(error.message);
+                    res.status(400).send({
+                      error: 'INTERNAL_ERROR',
+                    });
+                    return;
+                  }
 
-									res.json({
-										body: 'CORRECT',
-										challengeTries: challengeTries + 1,
-									});
-								}
-							);
-						})
-						.catch(error => {
-							res.json({ body: error, challengeTries: challengeTries + 1 });
-						});
-				}
-			);
-		}
-	);
+                  res.json({
+                    body: 'CORRECT',
+                    challengeTries: challengeTries + 1,
+                  });
+                }
+              );
+            })
+            .catch(error => {
+              res.json({ body: error, challengeTries: challengeTries + 1 });
+            });
+        }
+      );
+    }
+  );
 });
 
 app.listen(PORT, () => {
-	console.log(`Challenge API listening at http://localhost:${PORT}`);
+  console.log(`Challenge API listening at http://localhost:${PORT}`);
 });
--- challenge/constants.js
+++ challenge/constants.js
@@ -3,7 +3,7 @@ export const PORT = 4096;
 
 export const CHALLENGE_ALGORITHM = 'aes-256-ctr';
 export const CHALLENGE_PASSWORD =
-	process.env.CHALLENGE_PASSWORD || 'thisisapassword';
+  process.env.CHALLENGE_PASSWORD || 'thisisapassword';
 
 export const NUM_CHALLENGE_COURSES = 3; // number of courses to select for the challenge
 export const CHALLENGE_SEASON = '201903'; // season to select the challenge from
