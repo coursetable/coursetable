85b1c890a5aba7e4af9945c5a3885dc0888591ac
Max Yuan <maxyuan6717@gmail.com>
2020-10-15 23:32:46 -0400

    Redirect to worksheet page after signing in from worksheet login

--- frontend/src/pages/WorksheetLogin.js
+++ frontend/src/pages/WorksheetLogin.js
@@ -8,14 +8,14 @@ import Authentication from '../images/authentication.svg';
 function Thankyou() {
   return (
     <div className="text-center py-5">
-      <h3>Please <a href="/legacy_api/index.php?forcelogin=1">log in</a></h3>
+      <h3>Please <a href="/legacy_api/index.php?forcelogin=1&successurl=worksheet">log in</a></h3>
       <div>A valid Yale NetID is required to access worksheet information.</div>
       <img
         alt="Not logged in"
         className="py-5"
         src={Authentication}
         style={{ width: '25%' }}
-      ></img>    
+      ></img>
     </div>
   );
 }
