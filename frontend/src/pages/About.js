c5d50dff1b400584b13c86455859d75c826c5a14
Max Yuan <maxyuan6717@gmail.com>
2020-10-18 21:44:02 -0400

    fix prettier formatting

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -5,137 +5,128 @@ import { Card, CardDeck, Button, Row } from 'react-bootstrap';
 //import pic from '../images/default_pfp.png';
 import { Link } from 'react-router-dom';
 
-import ae from '../images/headshots/aidan-evans.jpg'
-import ml from '../images/headshots/michelle-li.jpg'
-import hs from '../images/headshots/harshal-sheth.jpg'
-import jc from '../images/headshots/josh-chough.jpg'
-import dl from '../images/headshots/deyuan-li.jpg'
-import kh from '../images/headshots/kevin-hu.jpg'
-import ma from '../images/headshots/murad-avliyakulov.jpg'
-import ag from '../images/headshots/abhijit-gupta.jpg'
-import my from '../images/headshots/max-yuan.jpg'
-import sh from '../images/headshots/sidney-hirschman.jpeg'
-import hl from '../images/headshots/hao-li.jpg'
-import df from '../images/headshots/dylan-fernandez-de-lara.jpg'
+import ae from '../images/headshots/aidan-evans.jpg';
+import ml from '../images/headshots/michelle-li.jpg';
+import hs from '../images/headshots/harshal-sheth.jpg';
+import jc from '../images/headshots/josh-chough.jpg';
+import dl from '../images/headshots/deyuan-li.jpg';
+import kh from '../images/headshots/kevin-hu.jpg';
+import ma from '../images/headshots/murad-avliyakulov.jpg';
+import ag from '../images/headshots/abhijit-gupta.jpg';
+import my from '../images/headshots/max-yuan.jpg';
+import sh from '../images/headshots/sidney-hirschman.jpeg';
+import hl from '../images/headshots/hao-li.jpg';
+import df from '../images/headshots/dylan-fernandez-de-lara.jpg';
 
 /**
  * Renders the about us page
  */
 
 function About() {
-
   const descriptions = [
-    (
-      <>
-        CourseTable was a course-data processor created by Peter Xu (Yale MC '14) and 
-        Harry Yu (Yale SY '14) and is continuing to be developed by the Yale Computer Society. 
-        It helps you find the courses at Yale where you'll learn and enjoy the most.
-      </>
-    ),
-    (
-      <>
-        After CourseTable's creation in 2013,{' '}
-        <a
-          href="https://coursetable.com/Blog"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Yale unceremonisously blocked it from campus networks.
-        </a>
-        {' '}Restrictions were later removed and as of 2019, the
-        Yale Computer Society has continued to maintain and further
-        develop the site.
-      </>
-    ),
-    (
-      <>
-        To learn more about how to use and the history of CourseTable please see{' '}
-        <Link to="/faq">
-          the FAQ page.
-        </Link>
-      </>
-    )
+    <>
+      CourseTable was a course-data processor created by Peter Xu (Yale MC '14)
+      and Harry Yu (Yale SY '14) and is continuing to be developed by the Yale
+      Computer Society. It helps you find the courses at Yale where you'll learn
+      and enjoy the most.
+    </>,
+    <>
+      After CourseTable's creation in 2013,{' '}
+      <a
+        href="https://coursetable.com/Blog"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Yale unceremonisously blocked it from campus networks.
+      </a>{' '}
+      Restrictions were later removed and as of 2019, the Yale Computer Society
+      has continued to maintain and further develop the site.
+    </>,
+    <>
+      To learn more about how to use and the history of CourseTable please see{' '}
+      <Link to="/faq">the FAQ page.</Link>
+    </>,
   ];
 
   const people = [
     {
-      name : 'Harshal Sheth',
+      name: 'Harshal Sheth',
       image: hs,
-      role : 'CourseTable Lead'
+      role: 'CourseTable Lead',
     },
     {
-      name : "Michelle M. Li",
+      name: 'Michelle M. Li',
       image: ml,
-      role : "Design Lead"
+      role: 'Design Lead',
     },
     {
-      name : "Kevin Hu",
+      name: 'Kevin Hu',
       image: kh,
-      role : 'Development'
+      role: 'Development',
     },
     {
-      name : "Max Yuan",
+      name: 'Max Yuan',
       image: my,
-      role : "Development"
+      role: 'Development',
     },
     {
-      name : "Murad Avliyakulov",
+      name: 'Murad Avliyakulov',
       image: ma,
-      role : "Development"
+      role: 'Development',
     },
     {
-      name : "Dylan Fernandez de Lara",
+      name: 'Dylan Fernandez de Lara',
       image: df,
-      role : "Design"
+      role: 'Design',
     },
     {
-      name : "Abhijit Gupta",
+      name: 'Abhijit Gupta',
       image: ag,
-      role : "Development"
+      role: 'Development',
     },
     {
-      name : "Sidney Hirschman",
+      name: 'Sidney Hirschman',
       image: sh,
-      role : "Design"
+      role: 'Design',
     },
     {
-      name : "Hao Li",
+      name: 'Hao Li',
       image: hl,
-      role : "Development"
+      role: 'Development',
     },
     {
-      name : "Josh Chough",
+      name: 'Josh Chough',
       image: jc,
-      role : "Design"
+      role: 'Design',
     },
     {
-      name : "Aidan Evans",
+      name: 'Aidan Evans',
       image: ae,
-      role : "Development"
+      role: 'Development',
     },
     {
-      name : "Deyuan Li",
+      name: 'Deyuan Li',
       image: dl,
-      role : "Development"
-    }
-  ]
-
+      role: 'Development',
+    },
+  ];
 
   //splits people into rows of three
-  var rows = [], size = 3;
+  var rows = [],
+    size = 3;
   for (var i = 0; i < people.length; i += size) {
-
     rows.push(people.slice(i, i + size));
-
   }
 
-  
   return (
     <div className={styles.container + ' mx-auto'}>
       <h1 className={styles.about_header + ' mt-5 mb-1'}>About Us</h1>
-      
+
       {descriptions.map((part, idx) => (
-        <p key={"description" + idx} className={styles.about_description + ' mb-3 text-muted'}>
+        <p
+          key={'description' + idx}
+          className={styles.about_description + ' mb-3 text-muted'}
+        >
           {part}
         </p>
       ))}
@@ -148,15 +139,13 @@ function About() {
 
       <div className={styles.profile_cards + ' my-3'}>
         {rows.map((row, idx) => (
-          <CardDeck key={idx} className={"my-3"}>
+          <CardDeck key={idx} className={'my-3'}>
             {row.map((person, idy) => (
               <Card key={idx + ' ' + idy}>
                 <Card.Img variant="top" src={person.image} />
                 <Card.Body>
                   <Card.Title>{person.name}</Card.Title>
-                  <Card.Text className="text-muted">
-                    {person.role}
-                  </Card.Text>
+                  <Card.Text className="text-muted">{person.role}</Card.Text>
                 </Card.Body>
               </Card>
             ))}
