671e195a85749bb9cd791ae374c668111a2637dd
Harshal Sheth <hsheth2@gmail.com>
2020-11-04 00:02:37 -0500

    Enable web vitals

--- frontend/src/Globals.js
+++ frontend/src/Globals.js
@@ -12,6 +12,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { InMemoryCache, ApolloClient } from '@apollo/client';
 import { ApolloProvider } from '@apollo/react-hooks';
 
+import reportWebVitals from './reportWebVitals';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
@@ -43,5 +45,10 @@ function Globals({ children }) {
     </>
   );
 }
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
 
 export default Globals;
