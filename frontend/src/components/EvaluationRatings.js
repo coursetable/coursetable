0f7ed1cd64784c0e8f049b5974bc558b28ac7b10
Max Yuan <maxyuan6717@gmail.com>
2020-11-11 16:04:00 -0500

    Add specific labels to eval graphs

--- frontend/src/components/EvaluationRatings.js
+++ frontend/src/components/EvaluationRatings.js
@@ -32,84 +32,83 @@ const EvaluationRatings = ({ crn, info }) => {
   });
 
   // Each of the following lists will store the values for a specific question
-  let assessment = [];
-  let workload = [];
-  let engagement = [];
-  let organized = [];
-  let feedback = [];
-  let challenge = [];
-  let major = [];
+  const questions = [
+    'assessment',
+    'workload',
+    'major',
+    'engagement',
+    'organized',
+    'feedback',
+    'challenge',
+  ];
+  const graph_labels = {
+    assessment: ['poor', 'fair', 'good', 'very good', 'excellent'],
+    workload: ['much less', 'less', 'same', 'greater', 'much greater'],
+    engagement: ['very low', 'low', 'medium', 'high', 'very high'],
+    organized: [
+      'strongly disagree',
+      'disagree',
+      'neutral',
+      'agree',
+      'strongly agree',
+    ],
+    feedback: [
+      'strongly disagree',
+      'disagree',
+      'neutral',
+      'agree',
+      'strongly agree',
+    ],
+    challenge: ['much less', 'less', 'same', 'greater', 'much greater'],
+    major: [],
+  };
+  const graph_titles = {
+    assessment: 'Overall',
+    workload: 'Workload',
+    engagement: 'Engagement',
+    organized: 'Organization',
+    feedback: 'Feedback Clarity',
+    challenge: 'Intellectual Challenge',
+    major: 'Taking for Major?',
+  };
+  let filtered_ratings = {
+    assessment: [],
+    workload: [],
+    engagement: [],
+    organized: [],
+    feedback: [],
+    challenge: [],
+    major: [],
+  };
   // Populate the lists above
   ratings.forEach((rating) => {
-    if (rating.question.includes('assessment')) assessment = rating.values;
-    else if (rating.question.includes('workload')) workload = rating.values;
-    else if (rating.question.includes('engagement')) engagement = rating.values;
-    else if (rating.question.includes('organized')) organized = rating.values;
-    else if (rating.question.includes('feedback')) feedback = rating.values;
-    else if (rating.question.includes('challenge')) challenge = rating.values;
-    else if (rating.question.includes('major')) major = rating.values;
+    questions.forEach((question) => {
+      if (rating.question.includes(question))
+        filtered_ratings[question] = rating.values;
+    });
   });
 
-  return (
-    <div>
-      {assessment.length > 0 && (
-        <div>
+  console.log(filtered_ratings);
+
+  let items = [];
+  questions.forEach((question) => {
+    if (filtered_ratings[question].length) {
+      items.push(
+        <div key={question}>
           <Row className="mx-auto mb-1 pl-1 justify-content-center">
-            <strong>Overall</strong>
+            <strong>{graph_titles[question]}</strong>
           </Row>
-          <RatingsGraph ratings={assessment} reverse={false} />
+          <RatingsGraph
+            ratings={filtered_ratings[question]}
+            reverse={question === 'major' || question === 'workload'}
+            labels={graph_labels[question]}
+          />
         </div>
-      )}
-      {workload.length > 0 && (
-        <div>
-          <Row className="mx-auto mb-1 pl-1 justify-content-center">
-            <strong>Workload</strong>
-          </Row>
-          <RatingsGraph ratings={workload} reverse={true} />
-        </div>
-      )}
-      {major.length > 0 && (
-        <div>
-          <Row className="mx-auto mb-1 pl-1 justify-content-center">
-            <strong>Taking for major?</strong>
-          </Row>
-          <RatingsGraph ratings={major} reverse={true} />
-        </div>
-      )}
-      {engagement.length > 0 && (
-        <div>
-          <Row className="mx-auto mb-1 pl-1 justify-content-center">
-            <strong>Engagement</strong>
-          </Row>
-          <RatingsGraph ratings={engagement} reverse={false} />
-        </div>
-      )}
-      {organized.length > 0 && (
-        <div>
-          <Row className="mx-auto mb-1 pl-1 justify-content-center">
-            <strong>Organization</strong>
-          </Row>
-          <RatingsGraph ratings={organized} reverse={false} />
-        </div>
-      )}
-      {feedback.length > 0 && (
-        <div>
-          <Row className="mx-auto mb-1 pl-1 justify-content-center">
-            <strong>Feedback clarity</strong>
-          </Row>
-          <RatingsGraph ratings={feedback} reverse={false} />
-        </div>
-      )}
-      {challenge.length > 0 && (
-        <div>
-          <Row className="mx-auto mb-1 pl-1 justify-content-center">
-            <strong>Intellectual Challenge</strong>
-          </Row>
-          <RatingsGraph ratings={challenge} reverse={false} />
-        </div>
-      )}
-    </div>
-  );
+      );
+    }
+  });
+
+  return <div>{items}</div>;
 };
 
 export default EvaluationRatings;
