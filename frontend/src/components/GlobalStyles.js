733f644de82b4a6445e21f1e824e4279d5c21d0d
Max Yuan <maxyuan6717@gmail.com>
2020-11-15 18:59:59 -0500

    Add contrast to a tags dark mode

--- frontend/src/components/GlobalStyles.js
+++ frontend/src/components/GlobalStyles.js
@@ -4,4 +4,13 @@ export const GlobalStyles = createGlobalStyle`
     background: ${({ theme }) => theme.background};
     color: ${({ theme }) => theme.text[0]};    
     transition: background 0.2s linear, color 0.2s linear;
-  }`;
+  }
+  a {
+    color: ${({ theme }) =>
+      theme.theme === 'light' ? '#007bff' : '#61adff'};  
+    &:hover {
+      color: ${({ theme }) =>
+        theme.theme === 'light' ? '#0066d4' : '#007bff'};  
+    }
+  }
+  `;
