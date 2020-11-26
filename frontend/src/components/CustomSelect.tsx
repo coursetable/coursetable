ae7b0755b8f6897ef5cda6af2f517be1cc55783c
Harshal Sheth <hsheth2@gmail.com>
2020-11-26 17:58:53 -0500

    setup custom select

--- frontend/src/components/CustomSelect.tsx
+++ frontend/src/components/CustomSelect.tsx
@@ -1,41 +1,53 @@
-import React, { useContext } from 'react';
-import { StyledSelect } from './StyledComponents';
-import { ThemeContext } from 'styled-components';
+import React from 'react';
+import { DefaultTheme, useTheme } from 'styled-components';
 import makeAnimated from 'react-select/animated';
 import chroma from 'chroma-js';
+import Select, {
+  OptionTypeBase,
+  Props as SelectProps,
+  StylesConfig,
+} from 'react-select';
+import { StylesConfigFunction } from 'react-select/src/styles';
 
 // Makes Select forms animated
 const animatedComponents = makeAnimated();
+
+interface Props extends SelectProps<OptionTypeBase> {
+  useColors: boolean;
+}
 
 /**
  * Custom Component for React-Select
  * @prop useColors - boolean | should we use the color version of styles?
  */
-
-function CustomSelect({ useColors = false, ...props }) {
-  const theme = useContext(ThemeContext);
+const CustomSelect: React.FC<Props> = ({ useColors = false, ...props }) => {
+  const theme = useTheme();
   const select_styles = selectStyles(theme);
   const select_styles_color = colorOptionStyles(theme);
   return (
-    <StyledSelect
+    <Select
       classNamePrefix={'Select'}
       components={animatedComponents}
       styles={useColors ? select_styles_color : select_styles}
       {...props}
     />
   );
-}
+};
 
 export default CustomSelect;
 
-const colorOptionStyles = (theme) => {
+const colorOptionStyles = (theme: DefaultTheme): StylesConfig => {
   return {
     control: (styles) => ({
       ...styles,
-      backgroundColor: 'white',
       borderRadius: '8px',
       cursor: 'pointer',
-      border: 'solid 2px rgba(0,0,0,0.1)',
+      backgroundColor: theme.select,
+      border:
+        theme.theme === 'light'
+          ? '2px solid hsl(0, 0%, 90%)'
+          : '2px solid ' + theme.select,
+      transition: 'background-color 0.2s linear, border 0.2s linear',
     }),
     option: (styles, { data, isDisabled, isFocused, isSelected }) => {
       const color = chroma(data.color);
@@ -59,12 +71,16 @@ const colorOptionStyles = (theme) => {
         cursor: isDisabled ? 'not-allowed' : 'pointer',
 
         ':active': {
-          ...styles[':active'],
+          ...(styles as any)[':active'],
           backgroundColor:
             !isDisabled && (isSelected ? data.color : color.alpha(0.5).css()),
         },
       };
     },
+    singleValue: (base) => ({
+      ...base,
+      transition: 'color 0.2s linear',
+    }),
     input: (base) => ({
       ...base,
       color: theme.text[0],
@@ -110,7 +126,7 @@ const colorOptionStyles = (theme) => {
   };
 };
 
-const selectStyles = (theme) => {
+const selectStyles = (theme: DefaultTheme): StylesConfig => {
   return {
     multiValue: (styles) => {
       return {
@@ -138,11 +154,13 @@ const selectStyles = (theme) => {
       borderRadius: '8px',
       cursor: 'pointer',
       border: 'solid 2px rgba(0,0,0,0.1)',
+      backgroundColor: theme.select,
+      transition: 'background-color 0.2s linear, border 0.2s linear',
     }),
     singleValue: (base, { isDisabled }) => ({
       ...base,
       color: isDisabled ? theme.text[2] : theme.text[0],
-      transition: '0.2s linear',
+      transition: 'color 0.2s linear',
     }),
     input: (base) => ({
       ...base,
@@ -164,7 +182,7 @@ const selectStyles = (theme) => {
       paddingBottom: 0,
       borderRadius: '8px',
     }),
-    option: (base, { isDisabled, isFocused, isSelected }) => ({
+    option: ((base, { isDisabled, isFocused, isSelected }) => ({
       ...base,
       cursor: 'pointer',
       backgroundColor: isDisabled
@@ -176,9 +194,9 @@ const selectStyles = (theme) => {
         : null,
       color: isSelected ? 'white' : theme.text[0],
       ':active': {
-        ...base[':active'],
+        ...(base as any)[':active'],
         backgroundColor: !isDisabled && '#85c2ff',
       },
-    }),
+    })) as StylesConfigFunction,
   };
 };
