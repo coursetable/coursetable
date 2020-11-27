83f9c2d9782ee883dea59eff0d779462de495014
Harshal Sheth <hsheth2@gmail.com>
2020-11-27 15:39:58 -0500

    fix gql import

--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -1,4 +1,4 @@
-import { gql } from 'graphql-tag';
+import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
