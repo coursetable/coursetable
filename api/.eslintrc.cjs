85738eb1b444b61deaa5c8d34e017b7a92602962
Harshal Sheth <hsheth2@gmail.com>
2020-12-12 18:35:12 -0500

    Enable eslint on api

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -3,12 +3,17 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['prettier', 'prettier/@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {},
 };
