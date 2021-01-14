417b6a2741e33c15111376dbda6f0a286554c0ac
Max Yuan <maxyuan6717@gmail.com>
2021-01-13 21:16:20 -0500

    Fix padding of search results container

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -852,7 +852,7 @@ function Search() {
           className={`m-0 ${
             isMobile
               ? `p-3 ${Styles.results_col_mobile}`
-              : (collapsed_form ? 'px-5 py-3 ' : 'px-0 py-3 ') +
+              : (collapsed_form ? 'px-5 pb-3 ' : 'px-0 pb-3 ') +
                 Styles.results_col
           }`}
         >
