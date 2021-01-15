8ef8528c47b8d5f58360ca4bab67dfcfddf3c361
Max Yuan <maxyuan6717@gmail.com>
2021-01-15 03:23:35 -0500

    Basic metadata stuff

--- frontend/src/components/CollapsableText.js
+++ frontend/src/components/CollapsableText.js
@@ -3,30 +3,40 @@ import { Collapse, Row } from 'react-bootstrap';
 import { StyledHr, TextComponent } from './StyledComponents';
 import styles from './CollapsableText.module.css';
 import { FiPlus, FiMinus } from 'react-icons/fi';
+import styled from 'styled-components';
 
-const CollapsableText = ({ header, body }) => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+const StyledToggle = styled(Row)`
+  justify-content: space-between;
+  cursor: pointer;
+  transition 0.1s background-color;
+  font-size: 14px;
+  &:hover {
+    background-color: ${({ theme }) => theme.select_hover};
+  }
+`;
+
+const CollapsableText = ({ header, body, init = true }) => {
+  const [isCollapsed, setIsCollapsed] = useState(init);
   return (
     <>
-      <Row className="mx-auto justify-content-between mt-3">
+      <StyledToggle
+        className="mx-auto mt-3"
+        onClick={() => {
+          setIsCollapsed(!isCollapsed);
+        }}
+      >
         <TextComponent type={2}>{header.toUpperCase()}</TextComponent>
-        <TextComponent
-          type={2}
-          onClick={() => {
-            setIsCollapsed(!isCollapsed);
-          }}
-          className={`${styles.toggle_btn} my-auto`}
-        >
+        <TextComponent type={2} className="my-auto">
           {isCollapsed ? (
             <FiPlus size={22} style={{ display: 'block' }} />
           ) : (
             <FiMinus size={22} style={{ display: 'block' }} />
           )}
         </TextComponent>
-      </Row>
+      </StyledToggle>
       <StyledHr className="my-0" />
       <Collapse in={!isCollapsed}>
-        <Row className="mx-auto mt-1">{body}</Row>
+        <Row className={`mx-auto mt-1 ${styles.body_text}`}>{body}</Row>
       </Collapse>
     </>
   );
--- frontend/src/components/NewCourseModalOverview.js
+++ frontend/src/components/NewCourseModalOverview.js
@@ -1,5 +1,12 @@
 import React, { useMemo, useState, useCallback } from 'react';
-import { Row, Col, Modal, OverlayTrigger, Popover } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Modal,
+  OverlayTrigger,
+  Popover,
+  Badge,
+} from 'react-bootstrap';
 import './MultiToggle.css';
 import LinesEllipsis from 'react-lines-ellipsis';
 import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC';
@@ -16,12 +23,16 @@ import {
 } from './StyledComponents';
 import { ratingColormap, workloadColormap } from '../queries/Constants';
 import Styles from './CourseModalOverview.module.css';
+import styles from './NewCourseModalOverview.module.css';
+import tag_styles from './SearchResultsItem.module.css';
 
 import CourseModalLoading from './CourseModalLoading';
 import CollapsableText from './CollapsableText';
 import { fbFriendsAlsoTaking, toSeasonString } from '../courseUtilities';
 import { useSearchAverageAcrossSeasonsQuery } from '../generated/graphql';
 import { weekdays } from '../common';
+import { skillsAreasColors } from '../queries/Constants';
+import chroma from 'chroma-js';
 
 // Button with season and other info that user selects to view evals
 const StyledCol = styled(Col)`
@@ -427,9 +438,13 @@ const CourseModalOverview = ({ setFilter, filter, setSeason, listing }) => {
         </Row>
       )}
       <Row className="mx-auto">
-        <Col md={8} className="pl-0 pr-2">
+        <Col md={8} className="pl-0 pr-4">
           {listing.description && (
-            <CollapsableText header="Description" body={listing.description} />
+            <CollapsableText
+              header="Description"
+              body={listing.description}
+              init={false}
+            />
           )}
           {listing.requirements && (
             <CollapsableText
@@ -456,8 +471,186 @@ const CourseModalOverview = ({ setFilter, filter, setSeason, listing }) => {
             />
           )}
         </Col>
-        <Col md={4} className="pr-0 pl-2">
-          Metadata
+        <Col md={4} className="pr-0 pl-4">
+          <Row className={`mx-auto mt-3 ${styles.metadata_header}`}>
+            <TextComponent type={2}>Professor</TextComponent>
+          </Row>
+          <Row className="mx-auto">
+            <span className={styles.metadata_body}>
+              {listing.professor_names.length
+                ? listing.professor_names.map((prof, index) => {
+                    return (
+                      <React.Fragment key={prof}>
+                        {index ? ' • ' : ''}
+                        <OverlayTrigger
+                          trigger="click"
+                          rootClose
+                          placement="top"
+                          overlay={renderProfInfoPopover}
+                          popperConfig={{ prof_name: prof }}
+                        >
+                          <StyledLink>{prof}</StyledLink>
+                        </OverlayTrigger>
+                      </React.Fragment>
+                    );
+                  })
+                : 'N/A'}
+            </span>
+          </Row>
+          <Row className={`mx-auto mt-3 ${styles.metadata_header}`}>
+            <TextComponent type={2}>Date & Time</TextComponent>
+          </Row>
+          <Row className="mx-auto">
+            <span className={styles.metadata_body}>
+              {listing.times_summary}
+            </span>
+          </Row>
+          <Row className={`mx-auto mt-3 ${styles.metadata_header}`}>
+            <TextComponent type={2}>Location</TextComponent>
+          </Row>
+          <Row className="mx-auto">
+            <span className={styles.metadata_body}>
+              {location_url !== '' ? (
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={location_url}
+                  className="d-flex"
+                >
+                  {listing.locations_summary}
+                  <HiExternalLink size={18} className="ml-1 my-auto" />
+                </a>
+              ) : (
+                listing.locations_summary
+              )}
+            </span>
+          </Row>
+          <Row className="mx-auto mt-3 justify-content-between">
+            {listing.areas && (
+              <Col xs="auto" className="p-0">
+                <Row className={`mx-auto ${styles.metadata_header}`}>
+                  <TextComponent type={2}>Areas</TextComponent>
+                </Row>
+                <Row className="mx-auto">
+                  {listing.areas.map((area) => (
+                    <Badge
+                      variant="secondary"
+                      className={tag_styles.tag}
+                      style={{
+                        color: skillsAreasColors[area],
+                        backgroundColor: chroma(skillsAreasColors[area])
+                          .alpha(0.16)
+                          .css(),
+                      }}
+                      key={area}
+                    >
+                      {area}
+                    </Badge>
+                  ))}
+                </Row>
+              </Col>
+            )}
+            {listing.skills && (
+              <Col xs="auto" className="p-0">
+                <Row className={`mx-auto ${styles.metadata_header}`}>
+                  <TextComponent type={2}>Skills</TextComponent>
+                </Row>
+                <Row className="mx-auto">
+                  {listing.skills.map((skill) => (
+                    <Badge
+                      variant="secondary"
+                      className={tag_styles.tag}
+                      style={{
+                        color: skillsAreasColors[skill],
+                        backgroundColor: chroma(skillsAreasColors[skill])
+                          .alpha(0.16)
+                          .css(),
+                      }}
+                      key={skill}
+                    >
+                      {skill}
+                    </Badge>
+                  ))}
+                </Row>
+              </Col>
+            )}
+            <Col xs="auto" className="p-0">
+              <Row className={`mx-auto ${styles.metadata_header}`}>
+                <TextComponent type={2}>Credits</TextComponent>
+              </Row>
+              <Row className="mx-auto">
+                <span className={styles.metadata_body}>
+                  {listing.credits.toFixed(1)}
+                </span>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="mx-auto mt-3 justify-content-between">
+            <Col xs="auto" className="p-0">
+              <Row className={`mx-auto ${styles.metadata_header}`}>
+                <TextComponent type={2}>Enrollment</TextComponent>
+              </Row>
+              <Row className="mx-auto">
+                <span className={styles.metadata_body}>
+                  {listing.enrolled
+                    ? listing.enrolled
+                    : listing.last_enrollment &&
+                      listing.last_enrollment_same_professors
+                    ? listing.last_enrollment
+                    : listing.last_enrollment
+                    ? `~${listing.last_enrollment} (different professor was teaching)`
+                    : 'N/A'}
+                </span>
+              </Row>
+            </Col>
+            {listing.syllabus_url && (
+              <Col xs="auto" className="p-0">
+                <Row className={`mx-auto ${styles.metadata_header}`}>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={listing.syllabus_url}
+                    className="d-flex"
+                  >
+                    Syllabus
+                    <HiExternalLink size={18} className="ml-1 my-auto" />
+                  </a>
+                </Row>
+              </Col>
+            )}
+          </Row>
+          {listing.flag_info.length > 0 && (
+            <>
+              <Row className={`mx-auto mt-3 ${styles.metadata_header}`}>
+                <TextComponent type={2}>Extra Info</TextComponent>
+              </Row>
+              <Row className="mx-auto">
+                <ul className={Styles.flag_info}>
+                  {listing.flag_info.map((text) => (
+                    <li key={text} className={styles.metadata_body}>
+                      {text}
+                    </li>
+                  ))}
+                </ul>
+              </Row>
+            </>
+          )}
+          {also_taking.length > 0 && (
+            <>
+              <Row className={`mx-auto mt-3 ${styles.metadata_header}`}>
+                <TextComponent type={2}>FB Friends</TextComponent>
+              </Row>
+              {also_taking.map((friend, index) => {
+                return (
+                  <Row className="m-auto" key={index}>
+                    <span className={styles.metadata_body}>
+                      {friend + (index === also_taking.length - 1 ? '' : ',')}
+                    </span>
+                  </Row>
+                );
+              })}
+            </>
+          )}
         </Col>
       </Row>
     </Modal.Body>
