98e1e7d7fd5801a455536f8ca0175d7393ea9c53
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-01-22 19:29:44 -0500

    Add season filter dropdown

--- frontend/src/components/NavbarSearch.tsx
+++ frontend/src/components/NavbarSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Form, InputGroup, Row } from 'react-bootstrap';
 import { GlobalHotKeys } from 'react-hotkeys';
 import { scroller } from 'react-scroll';
@@ -7,12 +7,21 @@ import styled from 'styled-components';
 import { useSessionStorageState } from '../browserStorage';
 import { StyledInput } from './StyledComponents';
 import { useWindowDimensions } from './WindowDimensionsProvider';
+import { NavbarSearchDropdown } from './NavbarSearchDropdown';
+import CustomSelect from './CustomSelect';
+import { useFerry } from './FerryProvider';
+import { ValueType } from 'react-select/src/types';
 
 const NavbarStyledSearchBar = styled(StyledInput)`
   border-radius: 4px;
   height: 100%;
   font-size: 14px;
 `;
+
+type Season = {
+  label: any;
+  value: any;
+};
 
 export const NavbarSearch: React.FC = () => {
   // Fetch width of window
@@ -23,6 +32,16 @@ export const NavbarSearch: React.FC = () => {
   // Search text for the default search if search bar was used
   const searchTextInput = useRef<HTMLInputElement>(null);
   const [searchText, setSearchText] = useSessionStorageState('searchText', '');
+
+  // Active dropdown
+  const [activeDropdown, setActiveDropdown] = useState('');
+  console.log('active dropdown: ', activeDropdown);
+
+  const defaultSeason: Season = { value: '202101', label: 'Spring 2021' };
+  const [
+    select_seasons,
+    setSelectSeasons,
+  ] = useSessionStorageState('select_seasons', [defaultSeason]);
 
   const scroll_to_results = useCallback(
     (event: React.FormEvent<HTMLFormElement>) => {
@@ -54,6 +73,24 @@ export const NavbarSearch: React.FC = () => {
     FOCUS_SEARCH: focusSearch,
   };
 
+  // populate seasons from database
+  let seasonsOptions;
+  const { seasons: seasonsData } = useFerry();
+  if (seasonsData && seasonsData.seasons) {
+    seasonsOptions = seasonsData.seasons.map((x) => {
+      return {
+        value: x.season_code,
+        // capitalize term and add year
+        label: `${x.term.charAt(0).toUpperCase() + x.term.slice(1)} ${x.year}`,
+      };
+    });
+  }
+
+  const handleSeasonChange = (options: Season[]) => {
+    // Set seasons state
+    setSelectSeasons(options || []);
+  };
+
   return (
     <>
       <GlobalHotKeys
@@ -82,6 +119,30 @@ export const NavbarSearch: React.FC = () => {
               />
             </InputGroup>
           </div>
+        </Row>
+        <Row className={`${styles.half_height} mx-auto`}>
+          <NavbarSearchDropdown
+            name="season"
+            placeholder="Season"
+            toggleText=""
+            setActiveDropdown={setActiveDropdown}
+          >
+            {/* Seasons Multi-Select */}
+            <CustomSelect
+              isMulti
+              keepMenuOpen
+              value={select_seasons}
+              options={seasonsOptions}
+              placeholder="Last 5 Years"
+              defaultMenuIsOpen
+              menuIsOpen={activeDropdown === 'season'}
+              // prevent overlap with tooltips
+              menuPortalTarget={document.body}
+              onChange={(selectedOption: ValueType<Season>) =>
+                handleSeasonChange(selectedOption as Season[])
+              }
+            />
+          </NavbarSearchDropdown>
         </Row>
       </Form>
     </>
