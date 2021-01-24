1c2a05d972d47010586df96ff54501645946aaf6
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-01-24 18:08:43 -0500

    Fix select styles for light/dark mode

--- frontend/src/components/PopoutSelect.tsx
+++ frontend/src/components/PopoutSelect.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import Select, { StylesConfig, Props as SelectProps } from 'react-select';
-
-const selectStyles = (width: number): StylesConfig => {
-  return {
-    control: (provided) => ({ ...provided, minWidth: width, margin: 8 }),
-    menu: () => ({ boxShadow: 'inset 0 1px 0 rgba(0, 0, 0, 0.1)' }),
-  };
-};
+import { Props as SelectProps } from 'react-select';
+import CustomSelect from './CustomSelect';
 
 export const PopoutSelect: React.FC<SelectProps> = ({ ...props }) => {
   return (
-    <Select
+    <CustomSelect
+      popout
       {...props}
-      styles={selectStyles(300)}
       autoFocus
       backspaceRemovesValue={false}
       // components={{ DropdownIndicator, IndicatorSeparator: null }}
       controlShouldRenderValue
-      hideSelectedOptions={false}
+      hideSelectedOptions
       isClearable
       menuIsOpen
       tabSelectsValue={false}
