85ccb7f9ed90b2568501b52167ecc651f74e9dc1
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-01-26 12:29:58 -0500

    Add subject & skills/areas filter dropdowns

--- frontend/src/components/PopoutSelect.tsx
+++ frontend/src/components/PopoutSelect.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Props as SelectProps } from 'react-select';
 import CustomSelect from './CustomSelect';
+// import { BiSearchAlt2 } from 'react-icons/bi';
+
+// const DropdownIndicator = (
+//   props: ElementConfig<typeof components.DropdownIndicator>
+// ) => {
+//   return (
+//     <components.DropdownIndicator {...props}>
+//       <BiSearchAlt2 />
+//     </components.DropdownIndicator>
+//   );
+// };
 
 export const PopoutSelect: React.FC<SelectProps> = ({ ...props }) => {
   return (
