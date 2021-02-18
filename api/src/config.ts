66e1b0fdfe9a47d6031dfbeb378038c4ecdf8288
ErikBoesen <me@erikboesen.com>
2021-02-17 21:55:50 -0500

    Remove challenge-related constants in API config

--- api/src/config.ts
+++ api/src/config.ts
@@ -15,14 +15,6 @@ export const MYSQL_DB_CONFIG = {
 export const GRAPHQL_ENDPOINT =
   process.env.GRAPHQL_ENDPOINT || die('graphql endpoint');
 
-export const CHALLENGE_ALGORITHM = 'aes-256-ctr';
-export const CHALLENGE_PASSWORD =
-  process.env.CHALLENGE_PASSWORD || die('challenge password');
-
-export const NUM_CHALLENGE_COURSES = 3; // number of courses to select for the challenge
-export const CHALLENGE_SEASON = '201903'; // season to select the challenge from
-export const MAX_CHALLENGE_REQUESTS = 100; // maximum number of allowed challenge tries
-
 // Secret for session cookie signing.
 export const SESSION_SECRET =
   process.env.SESSION_SECRET ?? die('session secret');
