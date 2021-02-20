18f9d2802263d11f76a97b7bca3034567e10c1a4
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-02-19 19:14:46 -0500

    Convert Search to TS

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useMemo,
-  useRef,
-} from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 
 import { Col, Container, Row, Form, InputGroup, Button } from 'react-bootstrap';
 import { Handle, Range } from 'rc-slider';
@@ -15,36 +9,23 @@ import styled from 'styled-components';
 import posthog from 'posthog-js';
 import Styles from './Search.module.css';
 
-import SearchResults from '../components/SearchResults';
+import CatalogResults from '../components/CatalogResults';
 import CourseModal from '../components/CourseModal';
 
 import {
-  areas,
-  skills,
   skillsAreasOptions,
-  creditOptions,
+  // creditOptions,
   schoolOptions,
   subjectOptions,
-  sortbyOptions,
-  AreasType,
-  SkillsType,
 } from '../queries/Constants';
 
 import { useWindowDimensions } from '../components/WindowDimensionsProvider';
-import { useCourseData, useFerry } from '../components/FerryProvider';
 import CustomSelect from '../components/CustomSelect';
 import SortByReactSelect from '../components/SortByReactSelect';
-import {
-  GroupedOptionsType,
-  OptionsType,
-  ValueType,
-} from 'react-select/src/types';
 
 import 'rc-slider/assets/index.css';
 import 'rc-tooltip/assets/bootstrap.css';
 
-import { useUser } from '../user';
-import { useSearch, Option, SortType, defaultFilters } from '../searchContext';
 import {
   SurfaceComponent,
   StyledInput,
@@ -52,8 +33,9 @@ import {
   TextComponent,
 } from '../components/StyledComponents';
 
-import { setSSObject, useSessionStorageState } from '../browserStorage';
-import { getNumFB, getOverallRatings, sortCourses } from '../courseUtilities';
+import { useSessionStorageState } from '../browserStorage';
+import { useSearch, Option } from '../searchContext';
+import { ValueType } from 'react-select/src/types';
 
 const StyledSearchTab = styled.div`
   background-color: ${({ theme }) =>
@@ -66,37 +48,16 @@ const StyledSearchTab = styled.div`
  * @prop history - dictionary that is used to reset default search value
  */
 
-function Search() {
-  // Fetch user context data
-  const { user } = useUser();
-  // Is the user logged in?
-  const isLoggedIn = user.worksheet != null;
-
-  // Object that holds a list of each fb friend taking a specific course
-  const num_fb = useMemo(() => {
-    if (!user.fbLogin || !user.fbWorksheets) return {};
-    return getNumFB(user.fbWorksheets);
-  }, [user.fbLogin, user.fbWorksheets]);
-
+const Search: React.FC = () => {
   // Fetch window dimensions
   const { height, width } = useWindowDimensions();
   const isMobile = width < 768;
-  // Search on page render?
-  const [defaultSearch, setDefaultSearch] = useState(true);
 
   // Is the search form  collapsed?
-  const [collapsed_form, setCollapsedForm] = useState(false);
+  const [collapsed_form, setCollapsedForm] = useState(true);
 
   // State that determines if a course modal needs to be displayed and which course to display
   const [course_modal, setCourseModal] = useState([false, '']);
-
-  // State that determines sort order
-  const defaultSort: SortType = 'asc';
-  const [ordering, setOrdering] = useSessionStorageState('ordering', {
-    course_code: defaultSort,
-  });
-  // State to reset sortby dropdown and rating sliders
-  const [reset_key, setResetKey] = useState(0);
 
   // Show the modal for the course that was clicked
   const showModal = useCallback(
@@ -132,10 +93,17 @@ function Search() {
     workloadBounds,
     select_seasons,
     select_schools,
-    select_credits,
+    // select_credits,
     hideCancelled,
     hideFirstYearSeminars,
     hideGraduateCourses,
+    seasonsOptions,
+    coursesLoading,
+    searchData,
+    multiSeasons,
+    reset_key,
+    isLoggedIn,
+    num_fb,
     setSearchText,
     setSelectSubjects,
     setSelectSkillsAreas,
@@ -145,309 +113,13 @@ function Search() {
     setWorkloadValueLabels,
     setSelectSeasons,
     setSelectSchools,
-    setSelectCredits,
+    // setSelectCredits,
     setHideCancelled,
     setHideFirstYearSeminars,
     setHideGraduateCourses,
+    setOrdering,
+    handleResetFilters,
   } = useSearch();
-
-  // populate seasons from database
-  let seasonsOptions:
-    | OptionsType<Option>
-    | GroupedOptionsType<Option>
-    | undefined;
-  const { seasons: seasonsData } = useFerry();
-  if (seasonsData && seasonsData.seasons) {
-    seasonsOptions = seasonsData.seasons.map((x) => {
-      return {
-        value: x.season_code,
-        // capitalize term and add year
-        label: `${x.term.charAt(0).toUpperCase() + x.term.slice(1)} ${x.year}`,
-      };
-    });
-  }
-
-  const required_seasons = useMemo(() => {
-    if (!isLoggedIn) {
-      // If we're not logged in, don't attempt to request any seasons.
-      return [];
-    }
-    if (select_seasons == null) {
-      return [];
-    }
-    if (select_seasons.length === 0) {
-      // Nothing selected, so default to all seasons.
-      return seasonsData.seasons.map((x) => x.season_code).slice(0, 15);
-    }
-    return select_seasons.map((x) => x.value);
-  }, [isLoggedIn, select_seasons, seasonsData]);
-
-  const {
-    loading: coursesLoading,
-    courses: courseData,
-    error: courseLoadError,
-  } = useCourseData(required_seasons);
-
-  // State used to determine whether or not to show season tags
-  // (if multiple seasons are queried, the season is indicated)
-  const multiSeasons = required_seasons.length !== 1;
-
-  const searchConfig = useMemo(() => {
-    // skills and areas
-    let processedSkillsAreas;
-    let processedSkills;
-    let processedAreas;
-    if (select_skillsareas != null) {
-      processedSkillsAreas = select_skillsareas.map((x) => {
-        return x.value;
-      });
-
-      // match all languages
-      if (processedSkillsAreas.includes('L')) {
-        processedSkillsAreas = processedSkillsAreas.concat([
-          'L1',
-          'L2',
-          'L3',
-          'L4',
-          'L5',
-        ]);
-      }
-
-      // separate skills and areas
-      processedSkills = processedSkillsAreas.filter((x: any): x is SkillsType =>
-        skills.includes(x)
-      );
-      processedAreas = processedSkillsAreas.filter((x: any): x is AreasType =>
-        areas.includes(x)
-      );
-
-      // set null defaults
-      if (processedSkills.length === 0) {
-        processedSkills = null;
-      }
-      if (processedAreas.length === 0) {
-        processedAreas = null;
-      }
-    }
-
-    // credits to filter
-    let processedCredits;
-    if (select_credits != null) {
-      processedCredits = select_credits.map((x) => {
-        return x.value;
-      });
-      // set null defaults
-      if (processedCredits.length === 0) {
-        processedCredits = null;
-      }
-    }
-
-    // schools to filter
-    let processedSchools;
-    if (select_schools != null) {
-      processedSchools = select_schools.map((x) => {
-        return x.value;
-      });
-
-      // set null defaults
-      if (processedSchools.length === 0) {
-        processedSchools = null;
-      }
-    }
-
-    // subject to filter
-    let processedSubjects;
-    if (select_subjects != null) {
-      processedSubjects = select_subjects.map((x) => {
-        return x.value;
-      });
-
-      // set null defaults
-      if (processedSubjects.length === 0) {
-        processedSubjects = null;
-      }
-    }
-
-    // if the bounds are unaltered, we need to set them to null
-    // to include unrated courses
-    const include_all_overalls =
-      overallBounds[0] === 1 && overallBounds[1] === 5;
-
-    const include_all_workloads =
-      workloadBounds[0] === 1 && workloadBounds[1] === 5;
-
-    // Variables to use in search query
-    const search_variables = {
-      search_text: searchText,
-      // seasons: not included because it is handled by required_seasons
-      areas: new Set(processedAreas),
-      skills: new Set(processedSkills),
-      credits: new Set(processedCredits),
-      schools: new Set(processedSchools),
-      subjects: new Set(processedSubjects),
-      min_overall: include_all_overalls ? null : overallBounds[0],
-      max_overall: include_all_overalls ? null : overallBounds[1],
-      min_workload: include_all_workloads ? null : workloadBounds[0],
-      max_workload: include_all_workloads ? null : workloadBounds[1],
-      extra_info: hideCancelled ? 'ACTIVE' : null,
-      fy_sem: hideFirstYearSeminars ? false : null,
-      grad_level: hideGraduateCourses ? false : null,
-    };
-
-    // Track search
-    posthog.capture('search', {
-      ...search_variables,
-      search_text_clean: search_variables.search_text || '[none]',
-    });
-
-    return search_variables;
-  }, [
-    hideCancelled,
-    hideFirstYearSeminars,
-    hideGraduateCourses,
-    overallBounds,
-    select_credits,
-    select_schools,
-    select_skillsareas,
-    select_subjects,
-    workloadBounds,
-    searchText,
-  ]);
-
-  const searchData = useMemo(() => {
-    // Match search results with course data.
-    if (coursesLoading || courseLoadError) return [];
-    if (Object.keys(searchConfig).length === 0) return [];
-
-    // Pre-processing for the search text.
-    const tokens = (searchConfig.search_text || '')
-      .split(/\s+/)
-      .filter((x) => !!x)
-      .map((token) => token.toLowerCase());
-
-    const listings = required_seasons
-      .map((season_code) => {
-        if (!courseData[season_code]) return [];
-        return [...courseData[season_code].values()];
-      })
-      .reduce((acc, cur) => acc.concat(cur));
-
-    const filtered = listings.filter((listing) => {
-      // Apply filters.
-      const average_overall = Number(getOverallRatings(listing));
-      if (
-        searchConfig.min_overall !== null &&
-        searchConfig.max_overall !== null &&
-        (average_overall === null ||
-          average_overall < searchConfig.min_overall ||
-          average_overall > searchConfig.max_overall)
-      ) {
-        return false;
-      }
-
-      if (
-        searchConfig.min_workload !== null &&
-        searchConfig.max_workload !== null &&
-        (listing.average_workload === null ||
-          listing.average_workload < searchConfig.min_workload ||
-          listing.average_workload > searchConfig.max_workload)
-      ) {
-        return false;
-      }
-
-      if (
-        searchConfig.extra_info !== null &&
-        searchConfig.extra_info !== listing.extra_info
-      ) {
-        return false;
-      }
-
-      if (
-        searchConfig.fy_sem !== null &&
-        searchConfig.fy_sem !== listing.fysem
-      ) {
-        return false;
-      }
-
-      if (
-        searchConfig.grad_level !== null &&
-        (listing.number === null ||
-          // tests if first character is between 5-9
-          (listing.number.charAt(0) >= '5' &&
-            listing.number.charAt(0) <= '9') ||
-          // otherwise if first character is not a number (i.e. summer classes), tests whether second character between 5-9
-          ((listing.number.charAt(0) < '0' || listing.number.charAt(0) > '9') &&
-            (listing.number.length <= 1 ||
-              (listing.number.charAt(1) >= '5' &&
-                listing.number.charAt(1) <= '9'))))
-      ) {
-        return false;
-      }
-
-      if (
-        searchConfig.subjects.size !== 0 &&
-        !searchConfig.subjects.has(listing.subject)
-      ) {
-        return false;
-      }
-
-      if (
-        (searchConfig.areas.size !== 0 || searchConfig.skills.size !== 0) &&
-        !listing.areas.some((v: any): v is AreasType =>
-          searchConfig.areas.has(v)
-        ) &&
-        !listing.skills.some((v: any): v is SkillsType =>
-          searchConfig.skills.has(v)
-        )
-      ) {
-        return false;
-      }
-
-      if (
-        searchConfig.credits.size !== 0 &&
-        listing.credits !== null &&
-        !searchConfig.credits.has(String(listing.credits))
-      ) {
-        return false;
-      }
-
-      if (
-        searchConfig.schools.size !== 0 &&
-        listing.school !== null &&
-        !searchConfig.schools.has(listing.school)
-      ) {
-        return false;
-      }
-
-      // Handle search text. Each token must match something.
-      for (const token of tokens) {
-        if (
-          listing.subject.toLowerCase().startsWith(token) ||
-          listing.number.toLowerCase().startsWith(token) ||
-          listing.title.toLowerCase().includes(token) ||
-          listing.professor_names.some((professor) =>
-            professor.toLowerCase().includes(token)
-          )
-        )
-          continue;
-
-        return false;
-      }
-
-      return true;
-    });
-
-    // Apply sorting order.
-    return sortCourses(filtered, ordering, num_fb);
-  }, [
-    required_seasons,
-    coursesLoading,
-    courseLoadError,
-    courseData,
-    searchConfig,
-    ordering,
-    num_fb,
-  ]);
 
   const handleSetView = useCallback(
     (isList) => {
@@ -503,24 +175,6 @@ function Search() {
   // Is the search form taller than the window?
   const [tooTall, setTooTall] = React.useState(true);
 
-  // reset the search form
-  const handleResetFilters = () => {
-    setHideCancelled(true);
-    setHideFirstYearSeminars(false);
-    setHideGraduateCourses(false);
-    setOverallBounds(defaultFilters.defaultOverallBounds);
-    setWorkloadBounds(defaultFilters.defaultWorkloadBounds);
-    setSelectSeasons(defaultFilters.defaultSeason);
-    setSelectSkillsAreas(defaultFilters.defaultOptions);
-    setSelectCredits(defaultFilters.defaultOptions);
-    setSelectSchools(defaultFilters.defaultOptions);
-    setSelectSubjects(defaultFilters.defaultOptions);
-
-    setSSObject('select_sortby', sortbyOptions[0]);
-    setSSObject('sort_order', 'asc');
-    setResetKey(reset_key + 1);
-  };
-
   // check if the search form is too tall
   // to be sticky
   const searchCol = useRef<HTMLFormElement>(null);
@@ -530,14 +184,6 @@ function Search() {
       setTooTall(searchColHeight > height - 56);
     }
   }, [setTooTall, height]);
-
-  // perform default search on load
-  useEffect(() => {
-    // only execute after seasons have been loaded
-    if (defaultSearch && seasonsOptions) {
-      setDefaultSearch(false);
-    }
-  }, [seasonsOptions, defaultSearch]);
 
   // Switch to grid view if window width changes < 900
   useEffect(() => {
@@ -552,6 +198,7 @@ function Search() {
           !isMobile ? 'd-flex flex-row-reverse flex-nowrap' : ''
         }`}
       >
+        {/* Search Form */}
         <Col
           md={3}
           className={
@@ -569,7 +216,6 @@ function Search() {
               !isMobile && !tooTall ? Styles.sticky : ''
             }`}
           >
-            {/* Search Form */}
             <Form className="px-0" onSubmit={scroll_to_results} ref={searchCol}>
               {!isMobile && (
                 // Render buttons to hide/show the search form
@@ -666,8 +312,8 @@ function Search() {
                     }
                   />
                 </div>
-                <div className={`col-md-12 p-0 ${Styles.selector_container}`}>
-                  {/* Course Credit Multi-Select */}
+                {/* <div className={`col-md-12 p-0 ${Styles.selector_container}`}>
+                  Course Credit Multi-Select
                   <CustomSelect
                     isMulti
                     value={select_credits}
@@ -679,7 +325,7 @@ function Search() {
                       setSelectCredits((selectedOption as Option[]) || []);
                     }}
                   />
-                </div>
+                </div> */}
                 <div className={`col-md-12 p-0 ${Styles.selector_container}`}>
                   {/* Yale Subjects Multi-Select */}
                   <CustomSelect
@@ -818,19 +464,18 @@ function Search() {
             </Form>
           </SurfaceComponent>
         </Col>
-        {/* Search Results Catalog */}
 
+        {/* Search Results Catalog */}
         <Col
           md={collapsed_form ? 12 : 9}
           className={`m-0 ${
             isMobile
               ? `p-3 ${Styles.results_col_mobile}`
-              : (collapsed_form ? 'px-5 pb-3 ' : 'px-0 pb-3 ') +
-                Styles.results_col
+              : `px-0 pb-3 ${Styles.results_col}`
           }`}
         >
-          <Element name="catalog">
-            <SearchResults
+          <Element name="catalog" className="d-flex justify-content-center">
+            <CatalogResults
               data={searchData}
               isList={isList}
               setView={handleSetView}
@@ -852,6 +497,6 @@ function Search() {
       />
     </div>
   );
-}
+};
 
 export default Search;
