3452ffc4809a2b4f17d9a86ed9751a40180c06b7
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-02-23 21:24:59 -0500

    Make color transitions a bit faster

--- frontend/src/components/Popout.tsx
+++ frontend/src/components/Popout.tsx
@@ -34,8 +34,7 @@ const StyledButton = styled.div`
   font-size: 14px;
   ${breakpoints('font-size', 'px', [{ 1320: 12 }])};
   border-radius: 4px;
-  transition: background-color 0.2s linear, border 0.2s linear,
-    color 0.2s linear;
+  transition: border 0.2s linear, color 0.2s linear;
   user-select: none;
   cursor: pointer;
 
