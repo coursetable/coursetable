a40f1594d5762f6d4b49e2e9303e078f6aa08f36
Kevin Hu <kevinhuwest@gmail.com>
2021-03-05 16:24:47 -0800

    Add dev startup script

---
+++ frontend/start-dev.sh
@@ -0,0 +1,6 @@
+export REACT_APP_UMAMI_WEBSITE_ID='603e532b-7def-4cf0-ab2c-031157aa7966'
+export REACT_APP_POSTHOG_TOKEN='nothing'
+export NODE_ENV='development'
+
+yarn
+FORCE_COLOR=true yarn start | cat
