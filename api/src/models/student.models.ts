8ca346eb37a18f8a25926c3b0bc20f7d1511a1c8
Kevin Hu <kevinhuwest@gmail.com>
2021-03-05 18:24:51 -0800

    Update student models

--- api/src/models/student.models.ts
+++ api/src/models/student.models.ts
@@ -1,13 +1,16 @@
 import mysql from 'mysql';
 import mysql_db from './mysql_db.js';
 
-const Student = function (student) {};
+const Student = function (student?: any) {};
 
-Student.findOrCreate = (netid) => {
+Student.findOrCreate = (
+  netid: string,
+  result: (statusCode: number, err: any, data: any) => void
+) => {
   mysql_db.getConnection(function (err, connection) {
     if (err) {
       console.error(err);
-      return;
+      result(500, err, null);
     }
     connection.query(
       `SELECT * FROM StudentBluebookSettings WHERE netid=${mysql.escape(
@@ -29,15 +32,16 @@ Student.findOrCreate = (netid) => {
             (err, res) => {
               if (err) {
                 console.error('findOrCreate create error: ', err);
+                result(500, err, null);
                 return;
               }
 
-              return res;
+              result(200, null, res);
             }
           );
         }
 
-        return res[0];
+        result(200, null, res[0]);
       }
     );
   });
@@ -54,7 +58,10 @@ Student.findOrCreate = (netid) => {
  *
  * @return {response_code, error_message, response_data}
  */
-Student.getChallengeStatus = (netid, result) => {
+Student.getEvalsStatus = (
+  netid: string,
+  result: (statusCode: number, err: any, data: any) => void
+) => {
   mysql_db.getConnection(function (err, connection) {
     if (err) {
       console.error(err);
@@ -75,66 +82,17 @@ Student.getChallengeStatus = (netid, result) => {
 
         // affirm single user retrieved
         if (res.length !== 1) {
-          result(401, 'USER_NOT_FOUND', null);
-          return;
+          result(500, 'User does not exist', null);
         }
 
         const evaluationsEnabled = res[0]['evaluationsEnabled'];
 
         // check if already enabled
         if (evaluationsEnabled === 1) {
-          result(403, 'ALREADY_ENABLED', null);
-          return;
+          result(200, null, true);
         }
 
-        result(200, null, res[0]);
-      }
-    );
-  });
-};
-
-/**
- * Get the challenge attempt count and whether or not
- * evaluations are enabled for a user.
- *
- * Note that an error is raised if evaluations are enabled.
- *
- * @prop netid - student's netID
- * @prop result - return object
- *
- * @return {response_code, error_message, response_data}
- */
-Student.getEvalsStatus = (netid) => {
-  mysql_db.getConnection(function (err, connection) {
-    if (err) {
-      console.error(err);
-
-      return;
-    }
-    connection.query(
-      `SELECT evaluationsEnabled FROM StudentBluebookSettings WHERE netid=${mysql.escape(
-        netid
-      )}`,
-      (err, res) => {
-        connection.release(); //put connection back in pool
-        if (err) {
-          console.error('findChallenge error: ', err);
-          return;
-        }
-
-        // affirm single user retrieved
-        if (res.length !== 1) {
-          return;
-        }
-
-        const evaluationsEnabled = res[0]['evaluationsEnabled'];
-
-        // check if already enabled
-        if (evaluationsEnabled === 1) {
-          return true;
-        }
-
-        return false;
+        result(500, err, null);
       }
     );
   });
@@ -148,7 +106,10 @@ Student.getEvalsStatus = (netid) => {
  *
  * @return {response_code, error_message, success_boolean}
  */
-Student.enableEvaluations = (netid, result) => {
+Student.enableEvaluations = (
+  netid: string,
+  result: (statusCode: number, err: any, data: any) => void
+) => {
   mysql_db.getConnection(function (err, connection) {
     if (err) {
       console.error(err);
