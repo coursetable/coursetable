73e2c97063bee4e5097a8b15ee29a537d0c62690
Max Yuan <maxyuan6717@gmail.com>
2021-03-07 15:17:47 -0500

    Add beta test banner to the top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,6 +60,11 @@ function App({ themeToggler }) {
   }
   return (
     <>
+      <Notice>
+        Want to try out the latest features and shape the future of CourseTable?
+        Become a Beta Tester{' '}
+        <a href="https://forms.gle/UtD5YnZ7MzxYLTux6">here</a>!
+      </Notice>
       <Navbar isLoggedIn={isLoggedIn} themeToggler={themeToggler} />
       <Switch>
         {/* Home Page */}
