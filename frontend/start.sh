f983eeaea6a3a712d2e1f347cca157421ffad087
Kevin Hu <kevinhuwest@gmail.com>
2021-03-09 17:57:21 -0800

    Update frontend start script

--- frontend/start.sh
+++ frontend/start.sh
@@ -2,5 +2,7 @@ export REACT_APP_UMAMI_WEBSITE_ID='603e532b-7def-4cf0-ab2c-031157aa7966'
 export REACT_APP_POSTHOG_TOKEN='nothing'
 export NODE_ENV='development'
 
-yarn
-FORCE_COLOR=true yarn start | cat
+doppler setup -p coursetable -c dev
+
+doppler run yarn
+doppler run yarn start
