52ba2ece35fd43547a54820140f3587527b20a6d
Kevin Hu <kevinhuwest@gmail.com>
2021-04-15 17:58:00 -0700

    Configure graphiql fetch

--- frontend/src/pages/Graphiql.tsx
+++ frontend/src/pages/Graphiql.tsx
@@ -1,6 +1,3 @@
-// import { Provider } from 'react-redux';
-// import { Playground, store } from 'graphql-playground-react';
-
 import { API_ENDPOINT } from '../config';
 
 import GraphiQL from 'graphiql';
@@ -8,20 +5,13 @@ import { createGraphiQLFetcher } from '@graphiql/toolkit';
 
 const fetcher = createGraphiQLFetcher({
   url: `${API_ENDPOINT}/ferry/v1/graphql`,
+  fetch: (url, args) => {
+    return fetch(url, { ...args, credentials: 'include' });
+  },
 });
 
 const Graphiql = () => (
   <div style={{ height: '100vh' }}>
-    {/* <link
-      rel="stylesheet"
-      href="//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css"
-    />
-    <Provider store={store}>
-      <Playground
-        endpoint={`${API_ENDPOINT}/ferry/v1/graphql`}
-        settings={{ 'editor.theme': 'light', 'request.credentials': 'include' }}
-      />
-    </Provider> */}
     <link href="https://unpkg.com/graphiql/graphiql.min.css" rel="stylesheet" />
     <GraphiQL fetcher={fetcher} editorTheme="dracula" />
   </div>
