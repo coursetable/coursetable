a6b2cfb421c208575cb8dd075ce3399fb71aedca
Kevin Hu <kevinhuwest@gmail.com>
2021-11-06 10:59:18 -0400

    Fix variable interpolation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactSvgPlugin from 'vite-plugin-react-svg';
+import { injectHtml } from 'vite-plugin-html';
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), reactSvgPlugin()],
+  plugins: [
+    reactRefresh(),
+    reactSvgPlugin(),
+    injectHtml({ injectData: dotenv.config().parsed }),
+  ],
   build: {
     outDir: './build',
   },
