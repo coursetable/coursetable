47848bfbd0fd0702688771776599c5f2abb92411
Neil Song <song.neil.song@gmail.com>
2023-10-09 19:05:42 -0400

    chore: Docker refactor 🐳 (#1351)
    * yarn -> npm
    
    * fix prestart with npm
    
    * compatibility with npm run
    
    * last of npm compat
    
    * api docker refactored - prod untested
    
    * ignore legacy in prettier lint
    
    * correct entrypoint
    
    * backwards compat with old prod docker
    
    * twice install
    
    ---------
    
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

---
+++ api/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$1" == 'dev' ]
+then
+    doppler setup -p coursetable -c dev
+    doppler run --command "docker-compose up --remove-orphans --build"
+elif [ "$1" == 'prod' ]
+then
+    doppler setup -p coursetable -c prd
+
+    VERSION=`sentry-cli releases propose-version`
+    export SENTRY_ORG=coursetable
+    export SENTRY_PROJECT=frontend
+
+    export SENTRY_RELEASE_VERSION=${VERSION}
+
+    sentry-cli releases new "$VERSION"
+    sentry-cli releases set-commits "$VERSION" --auto
+
+    doppler run --command "docker-compose -f docker-compose.yml -f prod-compose.yml build"
+    sentry-cli releases finalize "$VERSION"
+
+    doppler run --command "docker-compose -f docker-compose.yml -f prod-compose.yml up -d"
+    sentry-cli releases deploys "$VERSION" new -e production
+
+else
+    echo "Please use either 'dev' or 'prod', assuming 'dev' for this run."
+    doppler setup -p coursetable -c dev
+    doppler run --command "docker-compose -f docker-compose.yml -f dev-compose.yml up --remove-orphans --build"
+fi
