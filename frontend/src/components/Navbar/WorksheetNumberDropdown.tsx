42859af24fbad9deb532581eb344878fa9d5a12a
Joshua Chen <sidachen2003@gmail.com>
2023-11-07 20:24:49 -0500

    Fix lint

--- frontend/src/components/Navbar/WorksheetNumberDropdown.tsx
+++ frontend/src/components/Navbar/WorksheetNumberDropdown.tsx
@@ -1,50 +1,37 @@
 import React, { useMemo } from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
-import { NetId } from '../../utilities/common';
-import { useUser } from '../../contexts/userContext';
 import { useWorksheet } from '../../contexts/worksheetContext';
 import './DropdownShared.css';
 
-/**
- * Render FB Dropdown in mobile view.
- *
- * We include every friend in this list, even if they haven't selected
- * any classes in this season. In the list, we include the number of
- * listings they have in their worksheet in the given semester.
- */
 function WorksheetNumDropdown() {
-  // Fetch user context data
-  const { user } = useUser();
-
   const { changeWorksheet, worksheet_number } = useWorksheet();
 
   // Generate list of possible worksheets
-  const worksheet_options = useMemo(() => {
-    const worksheet_options_temp = [
+  const worksheetOptions = useMemo(() => {
+    const tempWorksheetOptions = [
       { value: '0', label: 'Main Worksheet' },
       { value: '1', label: 'Worksheet 1' },
       { value: '2', label: 'Worksheet 2' },
       { value: '3', label: 'Worksheet 3' },
     ];
-    return worksheet_options_temp;
+    return tempWorksheetOptions;
   }, []);
 
-  // worksheet drop downItem
   const WorksheetDropdownItem = ({
-    worksheet_number,
+    worksheetNumber,
   }: {
-    worksheet_number: string;
+    worksheetNumber: string;
   }) => {
     let text: string;
-    if (worksheet_number === '0') {
+    if (worksheetNumber === '0') {
       text = 'Main Worksheet';
     } else {
-      text = `Worksheet ${worksheet_number}`;
+      text = `Worksheet ${worksheetNumber}`;
     }
     return (
       <Dropdown.Item
-        key={worksheet_number}
-        eventKey={worksheet_number}
+        key={worksheetNumber}
+        eventKey={worksheetNumber}
         className="d-flex"
         // Styling if this is the current person
       >
@@ -63,14 +50,14 @@ function WorksheetNumDropdown() {
             ? 'Main Worksheet'
             : `Worksheet ${worksheet_number}`
         }
-        onSelect={(worksheet_number) => {
-          if (worksheet_number) {
-            changeWorksheet(worksheet_number);
+        onSelect={(worksheetNumber) => {
+          if (worksheetNumber) {
+            changeWorksheet(worksheetNumber);
           }
         }}
       >
-        {worksheet_options.map((worksheet) => (
-          <WorksheetDropdownItem worksheet_number={worksheet.value} />
+        {worksheetOptions.map((worksheet) => (
+          <WorksheetDropdownItem worksheetNumber={worksheet.value} />
         ))}
       </DropdownButton>
     </div>
