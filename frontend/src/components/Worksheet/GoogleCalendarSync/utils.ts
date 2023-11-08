4d14aa937de53204c1ae63da6d5c500b9d84a9ad
benzuzu <ben.xu@yale.edu>
2023-11-08 02:51:32 -0500

    Fix prettier errors

--- frontend/src/components/Worksheet/GoogleCalendarSync/utils.ts
+++ frontend/src/components/Worksheet/GoogleCalendarSync/utils.ts
@@ -8,7 +8,7 @@ const getISODateString = (day: number, time: string, reference: Date) => {
   const ret = new Date(
     reference.getFullYear(),
     reference.getMonth(),
-    reference.getDate() + ((day - reference.getDay() - 1) % 7 + 7) % 7 + 1 // positive remainder (1-7)
+    reference.getDate() + ((((day - reference.getDay() - 1) % 7) + 7) % 7) + 1, // positive remainder (1-7)
   );
 
   const [hourString, minuteString] = time.split(':');
@@ -55,7 +55,7 @@ const ISOtoICalFormat = (iso: string) => {
   return iso
     .replace(/[-:]/g, '') // Remove the hyphens and colons
     .substring(0, 15); // Remove timezone
-}
+};
 
 export const constructCalendarEvent = (course: Listing, colorIndex: number) => {
   if (course.times_summary === TBA_STRING) {
@@ -63,12 +63,17 @@ export const constructCalendarEvent = (course: Listing, colorIndex: number) => {
     return;
   }
 
-  const first_of_semester = course.season_code === "202303" ? new Date("2023-08-30") : new Date("2024-01-16");
-  const end_of_semester = course.season_code === "202303" ? "20231208T115959Z" : "20240426T115959Z";
+  const first_of_semester =
+    course.season_code === '202303'
+      ? new Date('2023-08-30')
+      : new Date('2024-01-16');
+  const end_of_semester =
+    course.season_code === '202303' ? '20231208T115959Z' : '20240426T115959Z';
 
   const { days, startTime, endTime } = getTimes(course.times_by_day);
 
-  let calendarStartTime, calendarEndTime = "";
+  let calendarStartTime,
+    calendarEndTime = '';
   for (const day of days) {
     if (day > first_of_semester.getDay()) {
       calendarStartTime = getISODateString(day, startTime, first_of_semester);
@@ -80,29 +85,36 @@ export const constructCalendarEvent = (course: Listing, colorIndex: number) => {
     calendarStartTime = getISODateString(days[0], startTime, first_of_semester);
     calendarEndTime = getISODateString(days[0], endTime, first_of_semester);
   }
-  
-  let breaks = "";
-  if (course.season_code === "202303") {
-    const fall_break = new Date("2023-10-18")
+
+  let breaks = '';
+  if (course.season_code === '202303') {
+    const fall_break = new Date('2023-10-18');
     for (const day of days) {
       if (day > fall_break.getDay()) {
-        breaks += ISOtoICalFormat(getISODateString(day, startTime, fall_break)) + ",";
+        breaks +=
+          ISOtoICalFormat(getISODateString(day, startTime, fall_break)) + ',';
       }
     }
 
-    const thanksgiving_break = new Date("2023-11-20");
+    const thanksgiving_break = new Date('2023-11-20');
     for (const day of days) {
-      breaks += ISOtoICalFormat(getISODateString(day, startTime, thanksgiving_break)) + ",";
+      breaks +=
+        ISOtoICalFormat(getISODateString(day, startTime, thanksgiving_break)) +
+        ',';
     }
-  } else if (course.season_code === "202401") {
-    const spring_break_w1 = new Date("2024-03-11");
+  } else if (course.season_code === '202401') {
+    const spring_break_w1 = new Date('2024-03-11');
     for (const day of days) {
-      breaks += ISOtoICalFormat(getISODateString(day, startTime, spring_break_w1)) + ",";
+      breaks +=
+        ISOtoICalFormat(getISODateString(day, startTime, spring_break_w1)) +
+        ',';
     }
-    
-    const spring_break_w2 = new Date("2024-03-18");
+
+    const spring_break_w2 = new Date('2024-03-18');
     for (const day of days) {
-      breaks += ISOtoICalFormat(getISODateString(day, startTime, spring_break_w2)) + ",";
+      breaks +=
+        ISOtoICalFormat(getISODateString(day, startTime, spring_break_w2)) +
+        ',';
     }
   }
 
@@ -119,7 +131,7 @@ export const constructCalendarEvent = (course: Listing, colorIndex: number) => {
   const byDay = days.map((day) => byDayMapping[day]).join(',');
 
   const event = {
-    id: 'coursetable' + uuidv4().replace(/-/g, ""),
+    id: 'coursetable' + uuidv4().replace(/-/g, ''),
     summary: course.course_code,
     start: {
       dateTime: calendarStartTime,
@@ -135,7 +147,7 @@ export const constructCalendarEvent = (course: Listing, colorIndex: number) => {
     ],
     colorId: (colorIndex + 1).toString(),
     description: course.title,
-    location: course.locations_summary
+    location: course.locations_summary,
   };
 
   return event;
