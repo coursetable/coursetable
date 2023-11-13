4c591aeb994b811e6dcd1db4fba504fdccf50fc2
Neil Song <song.neil.song@gmail.com>
2023-11-13 18:26:28 -0500

    chore: Dockerfile Refactor for Prisma Fix (#1382)
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>
    Co-authored-by: alex s <46074070+bearsyankees@users.noreply.github.com>

---
+++ api/entry.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Use hot reload env var to determine if we should run the server in hot reload mode
+if [ "$HOT_RELOAD" = "true" ]; then
+  # Run the server in hot reload mode
+  npm run start
+else
+  # Run the server in production mode
+  npm run prestart && npm run start:prod 
+fi
