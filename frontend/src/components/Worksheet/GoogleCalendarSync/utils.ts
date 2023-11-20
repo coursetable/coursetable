545eb0b530a6bbcb87125323ece51e189034d5fb
Ben Xu <71607240+benzuzu@users.noreply.github.com>
2023-11-20 16:41:29 -0600

    feat: Google Calendar integration (#1379)
    Co-authored-by: Bradley <bradleyevanlewis@gmail.com>
    Co-authored-by: alex s <46074070+bearsyankees@users.noreply.github.com>
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>
    Co-authored-by: Neil Song <song.neil.song@gmail.com>

---
+++ frontend/src/components/Worksheet/GoogleCalendarSync/utils.ts
@@ -0,0 +1,153 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Listing } from '../../Providers/FerryProvider';
+import moment from 'moment';
+
+const TBA_STRING = 'TBA';
+
+const getISODateString = (day: number, time: string, reference: Date) => {
+  const ret = new Date(
+    reference.getFullYear(),
+    reference.getMonth(),
+    reference.getDate() + ((((day - reference.getDay() - 1) % 7) + 7) % 7) + 1, // positive remainder (1-7)
+  );
+
+  const [hourString, minuteString] = time.split(':');
+  const hour = parseInt(hourString);
+  const minute = parseInt(minuteString);
+
+  ret.setHours(hour);
+  ret.setMinutes(minute);
+
+  return moment(ret).format();
+};
+
+const getTimes = (times_by_day: Listing['times_by_day']) => {
+  const daysMapping: Record<string, number> = {
+    Monday: 1,
+    Tuesday: 2,
+    Wednesday: 3,
+    Thursday: 4,
+    Friday: 5,
+    Saturday: 6,
+    Sunday: 0,
+  };
+
+  const days = [];
+
+  let startTime, endTime;
+
+  for (const day of Object.keys(times_by_day) as Array<
+    keyof typeof times_by_day
+  >) {
+    days.push(daysMapping[day]);
+    // Assuming each day starts and ends at the same time for now
+    [[startTime, endTime]] = times_by_day[day]!;
+  }
+
+  return {
+    days,
+    startTime: startTime!,
+    endTime: endTime!,
+  };
+};
+
+const ISOtoICalFormat = (iso: string) =>
+  iso
+    .replace(/[-:]/g, '') // Remove the hyphens and colons
+    .substring(0, 15); // Remove timezone
+
+export const constructCalendarEvent = (course: Listing, colorIndex: number) => {
+  if (course.times_summary === TBA_STRING) {
+    console.warn('TBA course', course.title);
+    return undefined;
+  }
+
+  const first_of_semester =
+    course.season_code === '202303'
+      ? new Date('2023-08-30')
+      : new Date('2024-01-16');
+  const end_of_semester =
+    course.season_code === '202303' ? '20231208T115959Z' : '20240426T115959Z';
+
+  const { days, startTime, endTime } = getTimes(course.times_by_day);
+
+  let calendarStartTime,
+    calendarEndTime = '';
+  for (const day of days) {
+    if (day > first_of_semester.getDay()) {
+      calendarStartTime = getISODateString(day, startTime, first_of_semester);
+      calendarEndTime = getISODateString(day, endTime, first_of_semester);
+      break;
+    }
+  }
+  if (!calendarStartTime) {
+    calendarStartTime = getISODateString(days[0], startTime, first_of_semester);
+    calendarEndTime = getISODateString(days[0], endTime, first_of_semester);
+  }
+
+  let breaks = '';
+  if (course.season_code === '202303') {
+    const fall_break = new Date('2023-10-18');
+    for (const day of days) {
+      if (day > fall_break.getDay()) {
+        breaks +=
+          ISOtoICalFormat(getISODateString(day, startTime, fall_break)) + ',';
+      }
+    }
+
+    const thanksgiving_break = new Date('2023-11-20');
+    for (const day of days) {
+      breaks +=
+        ISOtoICalFormat(getISODateString(day, startTime, thanksgiving_break)) +
+        ',';
+    }
+  } else if (course.season_code === '202401') {
+    const spring_break_w1 = new Date('2024-03-11');
+    for (const day of days) {
+      breaks +=
+        ISOtoICalFormat(getISODateString(day, startTime, spring_break_w1)) +
+        ',';
+    }
+
+    const spring_break_w2 = new Date('2024-03-18');
+    for (const day of days) {
+      breaks +=
+        ISOtoICalFormat(getISODateString(day, startTime, spring_break_w2)) +
+        ',';
+    }
+  }
+
+  const byDayMapping: Record<number, string> = {
+    0: 'SU',
+    1: 'MO',
+    2: 'TU',
+    3: 'WE',
+    4: 'TH',
+    5: 'FR',
+    6: 'SA',
+  };
+
+  const byDay = days.map((day) => byDayMapping[day]).join(',');
+
+  const event = {
+    id: 'coursetable' + uuidv4().replace(/-/g, ''),
+    summary: course.course_code,
+    start: {
+      dateTime: calendarStartTime,
+      timeZone: 'America/New_York',
+    },
+    end: {
+      dateTime: calendarEndTime,
+      timeZone: 'America/New_York',
+    },
+    recurrence: [
+      `RRULE:FREQ=WEEKLY;BYDAY=${byDay};UNTIL=${end_of_semester}`,
+      `EXDATE;TZID=America/New_York:${breaks}`,
+    ],
+    colorId: (colorIndex + 1).toString(),
+    description: course.title,
+    location: course.locations_summary,
+  };
+
+  return event;
+};
