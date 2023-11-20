6ab6a0cfd2f2f5d8339c1dc8b02bfd57b4b0c34f
alex s <46074070+bearsyankees@users.noreply.github.com>
2023-11-20 16:54:08 -0600

    Revert "feat: Google Calendar integration" (#1405)

--- frontend/src/pages/Graphiql.tsx
+++ frontend/src/pages/Graphiql.tsx
@@ -9,8 +9,7 @@ import './Graphiql.css';
 const fetcher = createGraphiQLFetcher({
   url: `${GRAPHQL_API_ENDPOINT}/v1/graphql`,
   fetch: (url, args) => {
-    // TODO @types/node and lib.dom are conflicting; we should try to exclude Node types
-    return fetch(url as never, { ...args, credentials: 'include' });
+    return fetch(url, { ...args, credentials: 'include' });
   },
 });
 
