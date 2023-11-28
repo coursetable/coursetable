10d84bcb754a5ff251753b1cced956c1228cfb6d
Joshua Chen <sidachen2003@gmail.com>
2023-11-28 13:03:42 -0500

    fix: do not "fix" class time before 8 (#1412)

--- frontend/src/components/Worksheet/WorksheetMobileCalendar.tsx
+++ frontend/src/components/Worksheet/WorksheetMobileCalendar.tsx
@@ -93,8 +93,8 @@ function WorksheetMobileCalendar() {
               const start = moment(startTime, 'HH:mm').day(1 + indx);
               const end = moment(endTime, 'HH:mm').day(1 + indx);
               // Fix any incorrect values
-              if (start.get('hour') < 8) start.add(12, 'h');
-              if (end.get('hour') < 8) end.add(12, 'h');
+              if (start.get('hour') < 7) start.add(12, 'h');
+              if (end.get('hour') < 7) end.add(12, 'h');
               const value = course.course_code;
               // Add event dictionary to the list
               parsedCourses.push({
