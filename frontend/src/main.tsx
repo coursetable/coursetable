eea89bd6dcbdea1d6af0d2b3c8c3f168309bf687
Joshua Chen <sidachen2003@gmail.com>
2023-12-24 21:29:44 +0800

    test: the dependencies have gone wild!

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Globals from './Globals';
+import _ from 'lodash';
 
 import App from './App';
 
 const domNode = document.getElementById('root')!;
 const root = createRoot(domNode);
+
+console.log(_);
 
 root.render(
   <Globals>
