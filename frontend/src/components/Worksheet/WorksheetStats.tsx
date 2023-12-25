d5d0b7975305bd63c208df5eddd33dbc1fee44fb
Joshua Chen <sidachen2003@gmail.com>
2023-12-25 23:29:51 +0800

    feat: display extra info when missing ratings

--- frontend/src/components/Worksheet/WorksheetStats.tsx
+++ frontend/src/components/Worksheet/WorksheetStats.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Collapse } from 'react-bootstrap';
+import { Collapse, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { MdInfoOutline } from 'react-icons/md';
 import styled from 'styled-components';
 import clsx from 'clsx';
 import chroma from 'chroma-js';
@@ -19,6 +20,10 @@ const StyledStatPill = styled.span<
   color: ${({ theme, stat }) => (stat ? '#141414' : theme.text[0])};
 `;
 
+const StyledInfo = styled(MdInfoOutline)`
+  color: ${({ theme }) => theme.primary};
+`;
+
 const courseNumberColormap = chroma
   .scale(['#63b37b', '#ffeb84', '#f8696b'])
   .domain([4, 6]);
@@ -34,22 +39,22 @@ export default function WorksheetStats() {
   const { courses, hiddenCourses, curSeason } = useWorksheet();
   const countedCourseCodes = new Set();
   let courseCnt = 0;
-  // Used to compute average
-  let coursesWithRating = 0;
   let credits = 0;
   let workload = 0;
   let rating = 0;
   const skillsAreas: string[] = [];
+  const coursesWithoutRating: string[] = [];
+  const coursesWithoutWorkload: string[] = [];
 
   for (const course of courses) {
     // See if any of the course's codes have already been counted or if it's
     // hidden so we don't double count
-    const shouldNotCount =
-      course.all_course_codes.some((code) => countedCourseCodes.has(code)) ||
-      hiddenCourses[curSeason]?.[course.crn];
-    const useCourseInfo = course.credits;
+    const alreadyCounted = course.all_course_codes.some((code) =>
+      countedCourseCodes.has(code),
+    );
+    const isHidden = Boolean(hiddenCourses[curSeason]?.[course.crn]);
 
-    if (shouldNotCount || !useCourseInfo) continue;
+    if (alreadyCounted || isHidden || !course.credits) continue;
 
     // Mark codes as counted, no double counting
     course.all_course_codes.forEach((code) => {
@@ -57,14 +62,16 @@ export default function WorksheetStats() {
     });
     const courseRating = getOverallRatings(course, 'stat');
     const courseWorkload = getWorkloadRatings(course, 'stat');
-
+    if (!courseRating) coursesWithoutRating.push(course.course_code);
+    if (!courseWorkload) coursesWithoutWorkload.push(course.course_code);
     courseCnt++;
-    coursesWithRating += courseRating ? 1 : 0;
-    credits += course.credits ?? 0;
+    credits += course.credits;
     workload += courseWorkload ?? 0;
     rating += courseRating ?? 0;
     skillsAreas.push(...course.skills, ...course.areas);
   }
+  const coursesWithWorkload = courseCnt - coursesWithoutWorkload.length;
+  const coursesWithRating = courseCnt - coursesWithoutRating.length;
 
   const avgRating = coursesWithRating === 0 ? 0 : rating / coursesWithRating;
   return (
@@ -98,13 +105,47 @@ export default function WorksheetStats() {
                 </StyledStatPill>
               </li>
               <li>
-                <StyledStatPill>Total workload</StyledStatPill>
+                <StyledStatPill>
+                  Total workload
+                  {coursesWithoutWorkload.length > 0 && (
+                    <OverlayTrigger
+                      placement="top"
+                      overlay={(props) => (
+                        <Tooltip {...props} id="conflict-icon-button-tooltip">
+                          <small style={{ fontWeight: 500 }}>
+                            Computed with {coursesWithWorkload} courses.{' '}
+                            {coursesWithoutWorkload.join(', ')} have no ratings.
+                          </small>
+                        </Tooltip>
+                      )}
+                    >
+                      <StyledInfo />
+                    </OverlayTrigger>
+                  )}
+                </StyledStatPill>
                 <StyledStatPill colormap={workloadColormap} stat={workload}>
                   {workload.toFixed(2)}
                 </StyledStatPill>
               </li>
               <li>
-                <StyledStatPill>Average rating</StyledStatPill>
+                <StyledStatPill>
+                  Average rating
+                  {coursesWithoutRating.length > 0 && (
+                    <OverlayTrigger
+                      placement="top"
+                      overlay={(props) => (
+                        <Tooltip {...props} id="conflict-icon-button-tooltip">
+                          <small style={{ fontWeight: 500 }}>
+                            Computed with {coursesWithRating} courses.{' '}
+                            {coursesWithoutRating.join(', ')} have no ratings.
+                          </small>
+                        </Tooltip>
+                      )}
+                    >
+                      <StyledInfo />
+                    </OverlayTrigger>
+                  )}
+                </StyledStatPill>
                 <StyledStatPill colormap={ratingColormap} stat={avgRating}>
                   {avgRating.toFixed(2)}
                 </StyledStatPill>
