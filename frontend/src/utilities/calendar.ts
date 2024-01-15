e0e9f1b8726632eb900f95c592c7f0f40ceaf455
Joshua Chen <sidachen2003@gmail.com>
2024-01-15 14:43:11 -0500

    Update frontend/src/utilities/calendar.ts

--- frontend/src/utilities/calendar.ts
+++ frontend/src/utilities/calendar.ts
@@ -139,6 +139,7 @@ function toGCalEvent({
       timeZone: 'America/New_York',
     },
     recurrence,
+    // Cycle the ID because each calendar only has a fixed set of colors
     colorId: ((colorIndex % 11) + 1).toString(),
     description,
     location,
