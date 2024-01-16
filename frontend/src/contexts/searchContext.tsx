0c9dd7046289c55b6080e617babc9da195b0ac34
Joshua Chen <sidachen2003@gmail.com>
2024-01-16 12:24:20 -0500

    refactor: get rid of searchConfig indirection

--- frontend/src/contexts/searchContext.tsx
+++ frontend/src/contexts/searchContext.tsx
@@ -6,11 +6,15 @@ import React, {
   useMemo,
   useState,
 } from 'react';
-import { isEqual, type Listing, type Season } from '../utilities/common';
+import {
+  isEqual,
+  type Listing,
+  type Season,
+  type Weekdays,
+} from '../utilities/common';
 import { useSessionStorageState } from '../utilities/browserStorage';
 import { useCourseData, useFerry, useWorksheetInfo } from './ferryContext';
 import {
-  skillsAreas,
   type SortByOption,
   sortbyOptions,
   type SortKeys,
@@ -50,7 +54,7 @@ export type OrderingType = {
 
 type Store = {
   filters: {
-    [K in keyof typeof defaultFilters]: ReturnType<typeof useFilterState<K>>;
+    [K in keyof Filters]: ReturnType<typeof useFilterState<K>>;
   };
   canReset: boolean;
   seasonsOptions: Option[];
@@ -75,32 +79,53 @@ export const defaultWorksheet: Option[] = [
   { value: '0', label: 'Main Worksheet' },
 ];
 
-export const defaultFilters = {
+type Filters = {
+  searchText: string;
+  selectSubjects: Option[];
+  selectSkillsAreas: Option[];
+  overallBounds: [number, number];
+  workloadBounds: [number, number];
+  selectSeasons: Option<Season>[];
+  selectDays: Option<Weekdays>[];
+  timeBounds: [string, string];
+  enrollBounds: [number, number];
+  numBounds: [number, number];
+  selectSchools: Option[];
+  selectCredits: Option<number>[];
+  searchDescription: boolean;
+  hideCancelled: boolean;
+  hideConflicting: boolean;
+  hideFirstYearSeminars: boolean;
+  hideGraduateCourses: boolean;
+  hideDiscussionSections: boolean;
+  selectSortby: SortByOption;
+  sortOrder: SortOrderType;
+};
+
+export const defaultFilters: Filters = {
   searchText: '',
-  selectSubjects: [] as Option[],
-  selectSkillsAreas: [] as Option[],
+  selectSubjects: [],
+  selectSkillsAreas: [],
   overallBounds: [1, 5],
   workloadBounds: [1, 5],
-  selectSeasons: [
-    { value: CUR_SEASON, label: toSeasonString(CUR_SEASON) },
-  ] as Option[],
-  selectDays: [] as Option[],
+  selectSeasons: [{ value: CUR_SEASON, label: toSeasonString(CUR_SEASON) }],
+  selectDays: [],
   timeBounds: ['7:00', '22:00'],
   enrollBounds: [1, 528],
   numBounds: [0, 1000],
-  selectSchools: [] as Option[],
-  selectCredits: [] as Option<number>[],
+  selectSchools: [],
+  selectCredits: [],
   searchDescription: false,
   hideCancelled: true,
   hideConflicting: false,
   hideFirstYearSeminars: false,
   hideGraduateCourses: false,
   hideDiscussionSections: true,
-  selectSortby: sortbyOptions[0] satisfies SortByOption as SortByOption,
-  sortOrder: 'asc' satisfies SortOrderType as SortOrderType,
+  selectSortby: sortbyOptions[0],
+  sortOrder: 'asc',
 };
 
-function useFilterState<K extends keyof typeof defaultFilters>(key: K) {
+function useFilterState<K extends keyof Filters>(key: K) {
   const [value, setValue] = useSessionStorageState(key, defaultFilters[key]);
   return useMemo(
     () => ({
@@ -177,90 +202,78 @@ export function SearchProvider({
     }),
   );
 
-  const requiredSeasons = useMemo((): Season[] => {
-    if (!isLoggedIn) {
-      // If we're not logged in, don't attempt to request any seasons.
-      return [];
-    }
+  // Pre-process queries
+  const processedSearchText = useMemo(
+    () =>
+      searchText.value
+        .split(/\s+/u)
+        .filter(Boolean)
+        .map((token) => token.toLowerCase()),
+    [searchText.value],
+  );
+  const processedSubjects = useMemo(
+    () => selectSubjects.value.map((x) => x.value),
+    [selectSubjects.value],
+  );
+  const processedSkillsAreas = useMemo(() => {
+    const ret = selectSkillsAreas.value.map((x) => x.value);
+    if (ret.includes('L')) ret.push('L1', 'L2', 'L3', 'L4', 'L5');
+    return ret;
+  }, [selectSkillsAreas.value]);
+  const processedSeasons = useMemo(() => {
     if (selectSeasons.value.length === 0) {
       // Nothing selected, so default to all seasons.
       return seasons.slice(0, 15);
     }
-    return selectSeasons.value.map((x) => x.value as Season);
-  }, [isLoggedIn, selectSeasons, seasons]);
+    return selectSeasons.value.map((x) => x.value);
+  }, [seasons, selectSeasons.value]);
+  const processedDays = useMemo(
+    () => selectDays.value.map((x) => x.value),
+    [selectDays.value],
+  );
+  const processedSchools = useMemo(
+    () => selectSchools.value.map((x) => x.value),
+    [selectSchools.value],
+  );
+  const processedCredits = useMemo(
+    () => selectCredits.value.map((x) => x.value),
+    [selectCredits.value],
+  );
+  // If the bounds are unaltered, we need to set them to null
+  // to include unrated courses
+  const processedOverallBounds = useMemo(
+    () => (overallBounds.hasChanged ? overallBounds.value : null),
+    [overallBounds],
+  );
+  const processedWorkloadBounds = useMemo(
+    () => (workloadBounds.hasChanged ? workloadBounds.value : null),
+    [workloadBounds],
+  );
+  const processedTimeBounds = useMemo(
+    () =>
+      timeBounds.hasChanged
+        ? (timeBounds.value.map(toRangeTime) as [number, number])
+        : null,
+    [timeBounds],
+  );
+  const processedEnrollBounds = useMemo(
+    () => (enrollBounds.hasChanged ? enrollBounds.value : null),
+    [enrollBounds],
+  );
+  const processedNumBounds = useMemo(
+    () => (numBounds.hasChanged ? numBounds.value : null),
+    [numBounds],
+  );
 
   const {
     loading: coursesLoading,
     courses: courseData,
     error: courseLoadError,
-  } = useCourseData(requiredSeasons);
+  } = useCourseData(processedSeasons);
 
   // State used to determine whether or not to show season tags
   // (if multiple seasons are queried, the season is indicated)
-  const multiSeasons = requiredSeasons.length !== 1;
-
-  // Search configuration of filters
-  const searchConfig = useMemo(() => {
-    // Skills and areas
-    const processedSkillsAreas = selectSkillsAreas.value.map((x) => x.value);
-    if (processedSkillsAreas.includes('L'))
-      processedSkillsAreas.push('L1', 'L2', 'L3', 'L4', 'L5');
-    const processedSkills = processedSkillsAreas.filter(
-      (x) => x in skillsAreas.skills,
-    );
-    const processedAreas = processedSkillsAreas.filter(
-      (x) => x in skillsAreas.areas,
-    );
-
-    // Variables to use in search query
-    const searchVariables = {
-      searchText: searchText.value,
-      // Seasons: not included because it is handled by required_seasons
-      areas: new Set(processedAreas),
-      skills: new Set(processedSkills),
-      credits: new Set(selectCredits.value.map((x) => x.value)),
-      schools: new Set(selectSchools.value.map((x) => x.value)),
-      subjects: new Set(selectSubjects.value.map((x) => x.value)),
-      days: new Set(selectDays.value.map((x) => x.value)),
-      // If the bounds are unaltered, we need to set them to null
-      // to include unrated courses
-      minOverall: overallBounds.hasChanged ? overallBounds.value[0] : null,
-      maxOverall: overallBounds.hasChanged ? overallBounds.value[1] : null,
-      minWorkload: workloadBounds.hasChanged ? workloadBounds.value[0] : null,
-      maxWorkload: workloadBounds.hasChanged ? workloadBounds.value[1] : null,
-      minTime: timeBounds.hasChanged ? timeBounds.value[0] : null,
-      maxTime: timeBounds.hasChanged ? timeBounds.value[1] : null,
-      minEnrollment: enrollBounds.hasChanged ? enrollBounds.value[0] : null,
-      maxEnrollment: enrollBounds.hasChanged ? enrollBounds.value[1] : null,
-      minNumber: numBounds.hasChanged ? numBounds.value[0] : null,
-      maxNumber: numBounds.hasChanged ? numBounds.value[1] : null,
-      description: searchDescription.value ? 'ACTIVE' : null,
-      extraInfo: hideCancelled.value ? 'ACTIVE' : null,
-      conflicting: hideConflicting.value ? 'ACTIVE' : null,
-      discussionSection: hideDiscussionSections.value ? 'ACTIVE' : null,
-      fySem: hideFirstYearSeminars.value ? false : null,
-      gradLevel: hideGraduateCourses.value ? false : null,
-    };
-    return searchVariables;
-  }, [
-    searchDescription,
-    hideCancelled,
-    hideConflicting,
-    hideFirstYearSeminars,
-    hideGraduateCourses,
-    hideDiscussionSections,
-    overallBounds,
-    workloadBounds,
-    timeBounds,
-    enrollBounds,
-    numBounds,
-    selectCredits,
-    selectSchools,
-    selectSkillsAreas,
-    selectSubjects,
-    selectDays,
-    searchText,
-  ]);
+  const multiSeasons = processedSeasons.length !== 1;
 
   const { data: worksheetInfo } = useWorksheetInfo(user.worksheet);
 
@@ -269,13 +282,7 @@ export function SearchProvider({
     // Match search results with course data.
     if (coursesLoading || courseLoadError) return [];
 
-    // Pre-processing for the search text.
-    const tokens = searchConfig.searchText
-      .split(/\s+/u)
-      .filter((x) => Boolean(x))
-      .map((token) => token.toLowerCase());
-
-    const listings = requiredSeasons
+    const listings = processedSeasons
       .map((seasonCode) => {
         if (!courseData[seasonCode]) return [];
         return [...courseData[seasonCode].values()];
@@ -284,71 +291,67 @@ export function SearchProvider({
 
     const filtered = listings.filter((listing) => {
       // Apply filters.
-      const averageOverall = getOverallRatings(listing, 'stat');
-      const overall =
-        averageOverall === null ? null : Math.round(averageOverall * 10) / 10;
-      if (
-        searchConfig.minOverall !== null &&
-        searchConfig.maxOverall !== null &&
-        (overall === null ||
-          overall < searchConfig.minOverall ||
-          overall > searchConfig.maxOverall)
-      )
-        return false;
+      if (processedOverallBounds !== null) {
+        const averageOverall = getOverallRatings(listing, 'stat');
+        const overall =
+          averageOverall === null ? null : Math.round(averageOverall * 10) / 10;
+        if (
+          overall === null ||
+          overall < processedOverallBounds[0] ||
+          overall > processedOverallBounds[1]
+        )
+          return false;
+      }
 
-      const averageWorkload = getWorkloadRatings(listing, 'stat');
-      const workload =
-        averageWorkload === null ? null : Math.round(averageWorkload * 10) / 10;
-      if (
-        searchConfig.minWorkload !== null &&
-        searchConfig.maxWorkload !== null &&
-        (workload === null ||
-          workload < searchConfig.minWorkload ||
-          workload > searchConfig.maxWorkload)
-      )
-        return false;
+      if (processedWorkloadBounds !== null) {
+        const averageWorkload = getWorkloadRatings(listing, 'stat');
+        const workload =
+          averageWorkload === null
+            ? null
+            : Math.round(averageWorkload * 10) / 10;
+        if (
+          workload === null ||
+          workload < processedWorkloadBounds[0] ||
+          workload > processedWorkloadBounds[1]
+        )
+          return false;
+      }
 
-      if (searchConfig.minTime !== null && searchConfig.maxTime !== null) {
+      if (processedTimeBounds !== null) {
         const times = getDayTimes(listing);
         if (
           !times.some(
             (time) =>
-              searchConfig.minTime !== null &&
-              searchConfig.maxTime !== null &&
-              toRangeTime(time.start) >= toRangeTime(searchConfig.minTime) &&
-              toRangeTime(time.end) <= toRangeTime(searchConfig.maxTime),
+              toRangeTime(time.start) >= processedTimeBounds[0] &&
+              toRangeTime(time.end) <= processedTimeBounds[1],
           )
         )
           return false;
       }
 
-      const enrollment = getEnrolled(listing, 'stat');
-      if (
-        searchConfig.minEnrollment !== null &&
-        searchConfig.maxEnrollment !== null &&
-        (enrollment === null ||
-          enrollment < searchConfig.minEnrollment ||
-          enrollment > searchConfig.maxEnrollment)
-      )
-        return false;
+      if (processedEnrollBounds !== null) {
+        const enrollment = getEnrolled(listing, 'stat');
+        if (
+          enrollment === null ||
+          enrollment < processedEnrollBounds[0] ||
+          enrollment > processedEnrollBounds[1]
+        )
+          return false;
+      }
 
-      const number = Number(listing.number.replace(/\D/gu, ''));
-      if (
-        searchConfig.minNumber !== null &&
-        searchConfig.maxNumber !== null &&
-        (number < searchConfig.minNumber ||
-          (searchConfig.maxNumber < 1000 && number > searchConfig.maxNumber))
-      )
-        return false;
+      if (processedNumBounds !== null) {
+        const number = Number(listing.number.replace(/\D/gu, ''));
+        if (
+          number < processedNumBounds[0] ||
+          (processedNumBounds[1] < 1000 && number > processedNumBounds[1])
+        )
+          return false;
+      }
 
-      if (
-        searchConfig.extraInfo !== null &&
-        searchConfig.extraInfo !== listing.extra_info
-      )
-        return false;
+      if (hideCancelled.value && listing.extra_info !== 'ACTIVE') return false;
 
       if (
-        searchConfig.conflicting !== null &&
+        hideConflicting.value &&
         listing.times_summary !== 'TBA' &&
         checkConflict(worksheetInfo, listing).length > 0
       )
@@ -356,17 +359,13 @@ export function SearchProvider({
 
       // Checks whether the section field consists only of letters -- if so, the
       // class is a discussion section.
-      if (
-        searchConfig.discussionSection !== null &&
-        /^[A-Z]*$/u.test(listing.section)
-      )
+      if (hideDiscussionSections.value && /^[A-Z]*$/u.test(listing.section))
         return false;
 
-      if (searchConfig.fySem !== null && searchConfig.fySem !== listing.fysem)
-        return false;
+      if (hideFirstYearSeminars.value && listing.fysem !== false) return false;
 
       if (
-        searchConfig.gradLevel !== null &&
+        hideGraduateCourses.value &&
         // Tests if first character is between 5-9
         ((listing.number.charAt(0) >= '5' && listing.number.charAt(0) <= '9') ||
           // Otherwise if first character is not a number (i.e. summer classes),
@@ -379,59 +378,54 @@ export function SearchProvider({
         return false;
 
       if (
-        searchConfig.subjects.size !== 0 &&
-        !searchConfig.subjects.has(listing.subject)
+        processedSubjects.length !== 0 &&
+        !processedSubjects.includes(listing.subject)
       )
         return false;
 
-      // TODO: searchConfig.days should be a literal set too
-      const days = new Set<string>(
-        getDayTimes(listing).map((daytime) => daytime.day),
-      );
-      if (searchConfig.days.size !== 0) {
-        let include = true as boolean;
-        days.forEach((day) => {
-          if (!searchConfig.days.has(day)) include = false;
-        });
-        searchConfig.days.forEach((day) => {
-          if (!days.has(day)) include = false;
-        });
-        if (!include) return false;
+      if (processedDays.length !== 0) {
+        const days = getDayTimes(listing).map((daytime) => daytime.day);
+        if (
+          days.some((day) => !processedDays.includes(day)) ||
+          processedDays.some((day) => !days.includes(day))
+        )
+          return false;
+      }
+
+      if (processedSkillsAreas.length !== 0) {
+        const listingSkillsAreas = [...listing.areas, ...listing.skills];
+        if (
+          !processedSkillsAreas.some((area) =>
+            listingSkillsAreas.includes(area),
+          )
+        )
+          return false;
       }
 
       if (
-        (searchConfig.areas.size !== 0 || searchConfig.skills.size !== 0) &&
-        !listing.areas.some((v) => searchConfig.areas.has(v)) &&
-        !listing.skills.some((v) => searchConfig.skills.has(v))
-      )
-        return false;
-
-      if (
-        searchConfig.credits.size !== 0 &&
+        processedCredits.length !== 0 &&
         listing.credits !== null &&
-        !searchConfig.credits.has(listing.credits)
+        !processedCredits.includes(listing.credits)
       )
         return false;
 
       if (
-        searchConfig.schools.size !== 0 &&
+        processedSchools.length !== 0 &&
         listing.school !== null &&
-        !searchConfig.schools.has(listing.school)
+        !processedSchools.includes(listing.school)
       )
         return false;
 
       // Handle search text. Each token must match something.
-      for (const token of tokens) {
+      for (const token of processedSearchText) {
         // First character of the course number
         const numberFirstChar = listing.number.charAt(0);
         if (
           listing.subject.toLowerCase().startsWith(token) ||
           listing.number.toLowerCase().startsWith(token) ||
-          // For course numbers that start with a letter (checked by if
-          // .toLowerCase() is not equal to .toUpperCase(), see
-          // https://stackoverflow.com/a/32567789/5540324),
+          // For course numbers that start with a letter,
           // exclude this letter when comparing with the search token
-          (numberFirstChar.toLowerCase() !== numberFirstChar.toUpperCase() &&
+          (/\D/u.test(numberFirstChar) &&
             listing.number
               .toLowerCase()
               .startsWith(numberFirstChar.toLowerCase() + token)) ||
@@ -456,16 +450,31 @@ export function SearchProvider({
       numFriends,
     );
   }, [
-    worksheetInfo,
-    searchDescription,
-    requiredSeasons,
     coursesLoading,
     courseLoadError,
-    courseData,
-    searchConfig,
-    selectSortby,
-    sortOrder,
+    processedSeasons,
+    selectSortby.value.value,
+    sortOrder.value,
     numFriends,
+    courseData,
+    processedOverallBounds,
+    processedWorkloadBounds,
+    processedTimeBounds,
+    processedEnrollBounds,
+    processedNumBounds,
+    hideCancelled.value,
+    hideConflicting.value,
+    worksheetInfo,
+    hideDiscussionSections.value,
+    hideFirstYearSeminars.value,
+    hideGraduateCourses.value,
+    processedSubjects,
+    processedDays,
+    processedSkillsAreas,
+    processedCredits,
+    processedSchools,
+    processedSearchText,
+    searchDescription.value,
   ]);
 
   const filters = useMemo(
