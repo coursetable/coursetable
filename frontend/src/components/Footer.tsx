08d93c641d2c19b35994af7f7525fd3e59b58a49
bearsyankees <bearsyankees@gmail.com>
2024-01-30 00:55:13 -0500

    normal buy me coffee commit

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -135,6 +135,16 @@ function Footer() {
                   <TextComponent type="secondary">LinkedIn</TextComponent>
                 </a>
               </li>
+                {/* Buy Me A Coffee */}
+                <li>
+                <a
+                  href="https://www.buymeacoffee.com/coursetable"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <TextComponent type="secondary">Support CourseTable</TextComponent>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
