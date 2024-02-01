57cb5e3f5595db9fa0f29afb3cf91f7ffde60b69
Joshua Chen <sidachen2003@gmail.com>
2024-02-01 14:16:43 -0500

    refactor: deduplicate some code

--- frontend/src/components/SkillBadge.tsx
+++ frontend/src/components/SkillBadge.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import chroma from 'chroma-js';
+import clsx from 'clsx';
 import { Badge } from 'react-bootstrap';
 import { skillsAreasColors } from '../utilities/constants';
 import styles from './SkillBadge.module.css';
@@ -7,14 +8,16 @@ import styles from './SkillBadge.module.css';
 export default function SkillBadge({
   skill,
   hidden,
+  className,
 }: {
   readonly skill: string;
   readonly hidden?: boolean;
+  readonly className?: string;
 }) {
   return (
     <Badge
       variant="secondary"
-      className={styles.tag}
+      className={clsx(className, styles.tag)}
       style={{
         color: skillsAreasColors[skill],
         backgroundColor: chroma(skillsAreasColors[skill]!).alpha(0.16).css(),
