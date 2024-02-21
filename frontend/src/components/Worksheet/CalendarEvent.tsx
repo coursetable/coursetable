df1a9bcc5bcfdedb00cbe0ea00dbb38a6c682b8a
Joshua Chen <sidachen2003@gmail.com>
2024-02-20 23:27:01 -0500

    No more JS

--- frontend/src/components/Worksheet/CalendarEvent.tsx
+++ frontend/src/components/Worksheet/CalendarEvent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
 import LinesEllipsis from 'react-lines-ellipsis';
 import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC';
@@ -14,55 +14,29 @@ const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 function CalendarEvent({ event }: { readonly event: RBCEvent }) {
   const course = event.listing;
   const { toggleCourse } = useWorksheet();
-  const [isHovering, setIsHovering] = useState(false);
 
   return (
-    <OverlayTrigger
-      placement="right"
-      show={isHovering} // Show popover based on hovering state
-      overlay={(props) => (
-        <InfoPopover {...props} id="title-popover">
-          <Popover.Title>
-            <strong>{course.title}</strong>
-            <span className="d-block">{course.times_summary}</span>
-          </Popover.Title>
-          <Popover.Content>
-            {truncatedText(course.description, 300, 'no description')}
-            <br />
-            <div className="text-danger">
-              {truncatedText(course.requirements, 250, '')}
-            </div>
-          </Popover.Content>
-        </InfoPopover>
-      )}
-      delay={{ show: 300, hide: 0 }}
-    >
-      <div
-        className={styles.event}
-        onMouseEnter={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
-        onFocus={() => setIsHovering(true)}
-        onBlur={() => setIsHovering(false)}
+    <>
+      <OverlayTrigger
+        placement="right"
+        overlay={(props) => (
+          <InfoPopover {...props} id="title-popover">
+            <Popover.Title>
+              <strong>{course.title}</strong>
+              <span className="d-block">{course.times_summary}</span>
+            </Popover.Title>
+            <Popover.Content>
+              {truncatedText(course.description, 300, 'no description')}
+              <br />
+              <div className="text-danger">
+                {truncatedText(course.requirements, 250, '')}
+              </div>
+            </Popover.Content>
+          </InfoPopover>
+        )}
+        delay={{ show: 300, hide: 0 }}
       >
-        <div
-          className={styles.worksheetHideButton}
-          // Prevent hovering over the hide button from showing the course info
-          // popover
-          onMouseEnter={(e) => {
-            e.stopPropagation();
-            setIsHovering(false);
-          }}
-          onMouseLeave={() => setIsHovering(true)}
-          onFocus={() => setIsHovering(false)}
-          onBlur={() => setIsHovering(true)}
-        >
-          <WorksheetHideButton
-            toggleCourse={() => toggleCourse(course.crn)}
-            // Course in calendar is never hidden
-            hidden={false}
-          />
-        </div>
-        <div>
+        <div className={styles.event}>
           <strong>{event.title}</strong>
           <br />
           <span>
@@ -75,8 +49,15 @@ function CalendarEvent({ event }: { readonly event: RBCEvent }) {
           </span>
           <small className={styles.locationText}>{event.location}</small>
         </div>
+      </OverlayTrigger>
+      <div className={styles.worksheetHideButton}>
+        <WorksheetHideButton
+          toggleCourse={() => toggleCourse(course.crn)}
+          // Course in calendar is never hidden
+          hidden={false}
+        />
       </div>
-    </OverlayTrigger>
+    </>
   );
 }
 
