1653ce4ef62ce531d60e8b2f602f5aad612750bf
bearsyankees <bearsyankees@gmail.com>
2024-02-26 23:51:01 -0500

    ok this is working now but is it safe

--- api/src/server.ts
+++ api/src/server.ts
@@ -115,24 +115,21 @@ https
 passportConfig(passport);
 app.use(passport.initialize());
 app.use(passport.authenticate('session'));
+app.use((req, res, next) => {
+  req.headers['X-Hasura-Role'] = req.isAuthenticated() ? 'student' : 'anonymous';
+  console.log("Forwarding role:", req.headers['X-Hasura-Role']);
+  next();
+});
 
-app.use(
-  '/ferry',
-  // Restrict GraphQL access for authenticated Yale students only
-  authWithEvals,
-  (req, res, next) => {
-    // Use read-only student role for all Hasura queries
-    req.headers['X-Hasura-Role'] = 'student';
-    next();
-  },
-  createProxyMiddleware({
-    target: 'http://graphql-engine:8080',
-    pathRewrite: {
-      '^/ferry/': '/', // Remove base path
-    },
-    ws: true,
-  }),
-);
+app.use('/ferry', createProxyMiddleware({
+  target: 'http://graphql-engine:8080',
+  pathRewrite: { '^/ferry/': '/' },
+  ws: true,
+  xfwd: true,
+  onProxyReq: (proxyReq, req, _) => {
+    const hasuraRole = req.headers['X-Hasura-Role'] || 'anonymous'; // default to 'anonymous'
+    proxyReq.setHeader('X-Hasura-Role', hasuraRole);  },
+}));
 // Enable request logging.
 app.use(morgan);
 
