31065695e1d0bcf63c649b433e6a8cad806914dd
alex s <46074070+bearsyankees@users.noreply.github.com>
2024-03-04 14:16:25 -0500

    2025 too

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -12,7 +12,7 @@ export const GRAPHQL_API_ENDPOINT = isDev
 export const CUR_SEASON = '202403' as Season;
 
 // Courses in the current year have no evaluations yet
-export const CUR_YEAR = ['202401', '202402', '202403'] as Season[];
+export const CUR_YEAR = ['202401', '202402', '202403', '202501'] as Season[];
 
 // We use this format to avoid dealing with time zones.
 // TODO: this should be a Temporal.ZonedDateTime
