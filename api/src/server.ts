b6974f3b4d5f80ecabd099ab732b0b175c79c536
bearsyankees <bearsyankees@gmail.com>
2024-03-06 00:54:15 -0500

    lint fix

--- api/src/server.ts
+++ api/src/server.ts
@@ -112,13 +112,13 @@ https
   });
 
 // Rate limit
-/* const authRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: 'Too many requests, please try again later',
-}); */
+// const authRateLimiter = rateLimit({
+// windowMs: 15 * 60 * 1000, // 15 minutes
+// max: 100, // Limit each IP to 100 requests per windowMs
+// standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+// legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// message: 'Too many requests, please try again later',
+// });
 
 // Configuring passport
 passportConfig(passport);
