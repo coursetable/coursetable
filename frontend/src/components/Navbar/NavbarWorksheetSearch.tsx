288b02cd9cc4a6a77cfb41a5e1fd888c254a4eb8
bearsyankees <bearsyankees@gmail.com>
2024-03-27 14:16:41 -0400

    ok removed earlier to pass lint

--- frontend/src/components/Navbar/NavbarWorksheetSearch.tsx
+++ frontend/src/components/Navbar/NavbarWorksheetSearch.tsx
@@ -202,7 +202,7 @@ function AddFriendDropdown({
           !isFriend(name.netId) &&
           ((name.first &&
             name.last &&
-            `${name.first ?? '[unknown]'} ${name.last ?? '[unknown]'}`
+            `${name.first} ${name.last}`
               .toLowerCase()
               .includes(searchText.toLowerCase())) ||
             name.netId.includes(searchText.toLowerCase())),
