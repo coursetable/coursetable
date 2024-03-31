a2ef2cb426034a014d7e0273539c8fe926745b94
Joshua Chen <sidachen2003@gmail.com>
2024-03-30 22:23:43 -0400

    refactor: get rid of a scrolling hack

--- frontend/src/components/Search/Results.tsx
+++ frontend/src/components/Search/Results.tsx
@@ -82,16 +82,20 @@ function Results({
     // window, the view can still be restored to list view
     resultsListing = (
       <WindowScroller>
-        {({ ref, outerRef, style: listStyle }) => (
+        {({ ref, outerRef }) => (
           // We use a list even for grid, because we only virtualize the rows
           <FixedSizeList
             outerRef={outerRef}
             ref={ref}
+            width={window.innerWidth}
             height={window.innerHeight}
             itemCount={Math.ceil(data.length / numCols)}
             itemSize={178}
-            width={window.innerWidth}
-            style={listStyle}
+            style={{
+              width: '100%',
+              height: '100%',
+              display: 'inline-block',
+            }}
           >
             {({ index, style: itemStyle }) => (
               <div style={itemStyle}>
@@ -116,16 +120,19 @@ function Results({
   } else {
     resultsListing = (
       <WindowScroller>
-        {({ ref, outerRef, style: listStyle }) => (
+        {({ ref, outerRef }) => (
           <FixedSizeList
             outerRef={outerRef}
             ref={ref}
+            width={window.innerWidth}
             height={window.innerHeight}
             itemCount={data.length}
             itemSize={isLgDesktop ? 32 : 28}
-            width={window.innerWidth}
-            style={listStyle}
-            useIsScrolling
+            style={{
+              width: '100%',
+              height: '100%',
+              display: 'inline-block',
+            }}
           >
             {({ index, style: itemStyle }) => (
               <ResultsItem
