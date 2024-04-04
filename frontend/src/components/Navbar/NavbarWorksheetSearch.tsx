bea572611041b0affeb53441ec7cf5626c567384
Joshua Chen <sidachen2003@gmail.com>
2024-04-04 15:01:23 -0400

    refactor: split out AddFriendDropdown

--- frontend/src/components/Navbar/NavbarWorksheetSearch.tsx
+++ frontend/src/components/Navbar/NavbarWorksheetSearch.tsx
@@ -1,232 +1,18 @@
-import React, { useMemo, useState, useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import clsx from 'clsx';
 import { Form, Row, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
-import {
-  components as selectComponents,
-  type SingleValueProps,
-  type OptionProps,
-} from 'react-select';
 import { toast } from 'react-toastify';
-import { MdPersonAdd, MdPersonRemove } from 'react-icons/md';
-import { Popout } from '../Search/Popout';
-import { PopoutSelect } from '../Search/PopoutSelect';
 import { LinkLikeText } from '../Typography';
 import FriendsDropdown from '../Worksheet/FriendsDropdown';
 import SeasonDropdown from '../Worksheet/SeasonDropdown';
 import WorksheetNumDropdown from '../Worksheet/WorksheetNumberDropdown';
+import AddFriendDropdown from '../Worksheet/AddFriendDropdown';
 
 import { useWorksheet } from '../../contexts/worksheetContext';
-import { fetchAllNames } from '../../utilities/api';
 import { useUser } from '../../contexts/userContext';
 import type { NetId } from '../../utilities/common';
 import styles from './NavbarWorksheetSearch.module.css';
 
-type FriendNames = {
-  netId: NetId;
-  first: string | null;
-  last: string | null;
-  college: string | null;
-}[];
-
-interface OptionType {
-  value: NetId;
-  label: string;
-  type: string;
-}
-
-function AddFriendDropdown({
-  removeFriend,
-}: {
-  readonly removeFriend: (netId: NetId, isRequest: boolean) => void;
-}) {
-  const { user, requestAddFriend, addFriend } = useUser();
-  const [allNames, setAllNames] = useState<FriendNames>([]);
-  const [searchText, setSearchText] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    async function fetchNames() {
-      const data = await fetchAllNames();
-      if (data) setAllNames(data.names as FriendNames);
-      setIsLoading(false);
-    }
-    void fetchNames();
-  }, []);
-  const isFriend = useMemo(() => {
-    const friends = new Set(Object.keys(user.friends ?? {}));
-    return (netId: NetId) => friends.has(netId);
-  }, [user.friends]);
-
-  const searchResults = useMemo(() => {
-    if (searchText.length < 3) return [];
-    return allNames
-      .filter(
-        (name) =>
-          name.netId !== user.netId &&
-          !isFriend(name.netId) &&
-          ((name.first &&
-            name.last &&
-            `${name.first} ${name.last}`
-              .toLowerCase()
-              .includes(searchText.toLowerCase())) ||
-            name.netId.includes(searchText.toLowerCase())),
-      )
-      .map((name) => ({
-        value: name.netId,
-        label: `${name.first ?? '[unknown]'} ${name.last ?? '[unknown]'} (${name.netId})`,
-        type: 'searchResult',
-      }));
-  }, [allNames, searchText, user.netId, isFriend]);
-  const friendRequestOptions = useMemo(
-    () =>
-      user.friendRequests?.map((request) => ({
-        value: request.netId,
-        label: request.name,
-        type: 'incomingRequest',
-      })) || [],
-    [user.friendRequests],
-  );
-
-  const customSingleValue = (props: SingleValueProps<OptionType, false>) => {
-    const { children, data } = props;
-
-    // Check if the selected value is a friend request or a search result that can be added
-    const isAddable = data.type === 'searchResult' && !isFriend(data.value);
-
-    return (
-      <selectComponents.SingleValue {...props}>
-        {children}
-        {isAddable && (
-          <MdPersonAdd
-            className={styles.addFriendIcon}
-            onClick={(e: React.MouseEvent) => {
-              e.stopPropagation(); // Prevent event from bubbling to the main select handler
-              void requestAddFriend(data.value);
-            }}
-            title="Send friend request"
-          />
-        )}
-      </selectComponents.SingleValue>
-    );
-  };
-
-  function OptionComponent(props: OptionProps<OptionType, false>) {
-    const { children, data, innerProps } = props;
-
-    const preventOptionSelection = (
-      e: React.MouseEvent | React.KeyboardEvent,
-    ) => {
-      e.preventDefault();
-      e.stopPropagation();
-    };
-
-    const addFriendClick = (e: React.MouseEvent) => {
-      e.stopPropagation();
-      void addFriend(data.value);
-    };
-
-    const requestFriendClick = (e: React.MouseEvent) => {
-      e.stopPropagation();
-      void requestAddFriend(data.value);
-    };
-
-    const removeFriendClick = (e: React.MouseEvent) => {
-      e.stopPropagation();
-      removeFriend(data.value, true);
-    };
-
-    if (data.type === 'searchResult') {
-      return (
-        <div
-          {...innerProps}
-          className={styles.friendOption}
-          role="button"
-          tabIndex={0}
-          onKeyDown={preventOptionSelection}
-          onClick={preventOptionSelection}
-        >
-          <span className={styles.friendOptionText}>{children}</span>
-          <MdPersonAdd
-            className={styles.addFriendIcon}
-            onClick={requestFriendClick}
-            title="Send friend request"
-          />
-        </div>
-      );
-    }
-
-    // For incoming requests
-    if (data.type === 'incomingRequest') {
-      return (
-        <div
-          {...innerProps}
-          className={styles.friendOption}
-          role="button"
-          tabIndex={0}
-          onKeyDown={preventOptionSelection}
-          onClick={preventOptionSelection}
-        >
-          <span className={styles.friendOptionText}>{children}</span>
-          <MdPersonAdd
-            className={styles.addFriendIcon}
-            onClick={addFriendClick}
-            title="Accept friend request"
-          />
-          <MdPersonRemove
-            className={styles.removeFriendIcon}
-            onClick={removeFriendClick}
-            title="Decline friend request"
-          />
-        </div>
-      );
-    }
-
-    return (
-      <selectComponents.Option {...props}>{children}</selectComponents.Option>
-    );
-  }
-
-  return (
-    <Popout buttonText="Add Friend" notifications={user.friendRequests?.length}>
-      <PopoutSelect
-        isClearable={false}
-        placeholder="Enter friend's name"
-        options={[
-          { label: 'Search Results', options: searchResults },
-          { label: 'Incoming Requests', options: friendRequestOptions },
-        ]}
-        onInputChange={(newValue) => setSearchText(newValue)}
-        components={{
-          Option: OptionComponent,
-          SingleValue: customSingleValue,
-          NoOptionsMessage({ children, ...props }) {
-            if (isLoading) {
-              return (
-                <selectComponents.NoOptionsMessage {...props}>
-                  Loading names...
-                </selectComponents.NoOptionsMessage>
-              );
-            }
-            return searchText.length < 3 ? (
-              <selectComponents.NoOptionsMessage {...props}>
-                Type at least 3 characters to search
-              </selectComponents.NoOptionsMessage>
-            ) : (
-              <selectComponents.NoOptionsMessage {...props}>
-                No results found
-              </selectComponents.NoOptionsMessage>
-            );
-          },
-        }}
-      />
-    </Popout>
-  );
-}
-
-/**
- * Worksheet search form for the desktop in the navbar
- */
 export function NavbarWorksheetSearch() {
   const { worksheetView, handleWorksheetView, person, handlePersonChange } =
     useWorksheet();
@@ -299,7 +85,10 @@ export function NavbarWorksheetSearch() {
               mobile={false}
               removeFriend={removeFriendWithConfirmation}
             />
-            <AddFriendDropdown removeFriend={removeFriendWithConfirmation} />
+            <AddFriendDropdown
+              mobile={false}
+              removeFriend={removeFriendWithConfirmation}
+            />
           </div>
         </Row>
       </Form>
