9464b912a4773844903611b15a16a5c65fecbc56
Neil Song <song.neil.song@gmail.com>
2024-04-03 22:05:19 -0400

    feat: back to node! (#1667)

--- api/entry.sh
+++ api/entry.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 cd $(dirname "$0")
-
+npm run generate
 # Use hot reload env var to determine if we should run the server in hot reload mode
 if [ "$HOT_RELOAD" = "true" ]; then
   # Run the server in hot reload mode
-  bun run start
+  npm run start
 else
   # Run the server in production mode
-  bun run start:prod
+  npm run start:prod
 fi
