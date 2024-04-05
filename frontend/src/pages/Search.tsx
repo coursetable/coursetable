8cf4d34c770421d8b0006ebed9e07209e5355461
Joshua Chen <sidachen2003@gmail.com>
2024-04-04 22:43:49 -0400

    refactor: remove useless react-scroll imports

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { Element, scroller } from 'react-scroll';
+import React from 'react';
+import { Element } from 'react-scroll';
 import 'rc-slider/assets/index.css';
 import 'rc-tooltip/assets/bootstrap.css';
 
@@ -14,35 +14,10 @@ function Search() {
   const { isMobile } = useWindowDimensions();
   const { coursesLoading, searchData, multiSeasons } = useSearch();
 
-  const scrollToResults = useCallback(
-    (event?: React.FormEvent) => {
-      if (event) event.preventDefault();
-
-      // Scroll down to catalog when in mobile view.
-      if (isMobile) {
-        scroller.scrollTo('catalog', {
-          smooth: true,
-          duration: 500,
-          offset: -56,
-        });
-      }
-    },
-    [isMobile],
-  );
-
-  // Scroll to the bottom when courses finish loading on initial load.
-  const [doneInitialScroll, setDoneInitialScroll] = useState(false);
-  useEffect(() => {
-    if (!coursesLoading && !doneInitialScroll) {
-      scrollToResults();
-      setDoneInitialScroll(true);
-    }
-  }, [coursesLoading, doneInitialScroll, scrollToResults]);
-
   // TODO: add state if courseLoadError is present
   return (
     <div className={styles.searchBase}>
-      {isMobile && <MobileSearchForm onSubmit={scrollToResults} />}
+      {isMobile && <MobileSearchForm />}
       <div className={styles.resultsCol}>
         <Element name="catalog" className="d-flex justify-content-center">
           <Results
