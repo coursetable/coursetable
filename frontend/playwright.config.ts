f028f0b2df8c5cb2e821bcb9ef41a2ed239c6226
Joshua Chen <sidachen2003@gmail.com>
2024-04-05 17:39:32 -0400

    Add env?

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from '@playwright/test';
 
+console.log('BASE_URL:', process.env.BASE_URL);
+
 export default defineConfig({
   use: {
+    baseURL: process.env.BASE_URL,
     extraHTTPHeaders: {
       'x-vercel-skip-toolbar': '1',
     },
