1c65ec7c66971b62405b31c793d927d9912000fe
Joshua Chen <sidachen2003@gmail.com>
2024-04-16 20:20:47 -0400

    refactor: remove duplicate code

--- frontend/src/components/Search/Toggle.tsx
+++ frontend/src/components/Search/Toggle.tsx
@@ -2,54 +2,47 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
 import { IoFlaskOutline } from 'react-icons/io5';
-import { useSearch, type Filters } from '../../contexts/searchContext';
+import {
+  useSearch,
+  filterLabels,
+  type BooleanFilters,
+} from '../../contexts/searchContext';
 import styles from './Toggle.module.css';
 
-const labels = {
-  searchDescription: 'Include descriptions in search',
-  enableQuist: [
-    // eslint-disable-next-line react/jsx-key
-    <>
-      Enable <IoFlaskOutline color="var(--color-primary)" />
-      <Link to="/releases/quist">Quist</Link>
-    </>,
-    'Enable Quist',
-  ],
-  hideCancelled: 'Hide cancelled courses',
-  hideConflicting: 'Hide courses with conflicting times',
-  hideFirstYearSeminars: 'Hide first-year seminars',
-  hideGraduateCourses: 'Hide graduate courses',
-  hideDiscussionSections: 'Hide discussion sections',
-} as const;
-
-export default function Toggle({
-  handle,
+export default function Toggle<K extends BooleanFilters>({
+  handle: handleName,
 }: {
-  readonly handle: {
-    [K in keyof Filters]: Filters[K] extends boolean ? K : never;
-  }[keyof Filters];
+  readonly handle: K;
 }) {
   const { filters, setStartTime } = useSearch();
-  const label = labels[handle];
+  const label = filterLabels[handleName];
+  const handle = filters[handleName];
   return (
     <Form.Check type="switch" className={styles.check}>
       <Form.Check.Input
-        aria-label={typeof label === 'string' ? label : label[1]}
+        aria-label={label}
         className={styles.input}
-        checked={filters[handle].value}
+        checked={handle.value}
         onChange={() => {
-          filters[handle].set(!filters[handle].value);
+          handle.set(!handle.value);
           setStartTime(Date.now());
         }}
       />
       <Form.Check.Label
         className={styles.label}
         onClick={() => {
-          filters[handle].set(!filters[handle].value);
+          handle.set(!handle.value);
           setStartTime(Date.now());
         }}
       >
-        {typeof label === 'string' ? label : label[0]}
+        {handleName === 'enableQuist' ? (
+          <>
+            Enable <IoFlaskOutline color="var(--color-primary)" />
+            <Link to="/releases/quist">Quist</Link>
+          </>
+        ) : (
+          filterLabels[handleName]
+        )}
       </Form.Check.Label>
     </Form.Check>
   );
