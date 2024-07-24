c9ef0a7094c49140b3ec1c1a8bf35b358c7a998d
Joshua Chen <sidachen2003@gmail.com>
2024-07-24 10:51:44 -0400

    Got condition wrong

--- frontend/src/components/markdown/Link.tsx
+++ frontend/src/components/markdown/Link.tsx
@@ -8,7 +8,7 @@ export default function MDLink({
   // eslint-disable-next-line react/jsx-no-target-blank
   if (!href) return <a {...props}>{children}</a>;
   const absolute = new URL(href, 'https://coursetable.com');
-  if (absolute.origin === 'https://coursetable.com') {
+  if (absolute.origin !== 'https://coursetable.com') {
     return (
       <a href={href} {...props} rel="noopener noreferrer" target="_blank">
         {children}
