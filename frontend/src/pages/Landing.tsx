d38e053bc92e3a6b358ff38495c82e088f7e6142
bearsyankees <bearsyankees@gmail.com>
2024-08-18 21:12:02 -0500

    feat: student reviews on landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import {
@@ -11,7 +12,40 @@ import { API_ENDPOINT } from '../config';
 import LandingImage from '../images/landing_page.svg';
 import styles from './Landing.module.css';
 
+const testimonials = [
+  "Thank you for making this website!! It's a life saver every semester.",
+  'I started off the semester with 80 courses in my course table! Thank you so much for your incredible website :)',
+  'Appreciate all that you do! Course table is a life saver',
+  'Thank you for making my life so much easier! Appreciate it immensely <3',
+  'This should not be free',
+  'CourseTable has been SOOOO helpful in helping me choose good classes over the years. I would been overwhelmed every semester without it.',
+  'Thank you for your service 🙏',
+  'U guys rock',
+];
+
 function Landing() {
+  const [currentTestimonial, setCurrentTestimonial] = useState(0);
+  const [slideDirection, setSlideDirection] = useState('slide-in');
+  const [containerHeight, setContainerHeight] = useState('auto');
+  const testimonialRef: React.MutableRefObject<HTMLParagraphElement | null> =
+    useRef(null);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSlideDirection('slide-out');
+      setTimeout(() => {
+        setCurrentTestimonial((prev) => (prev + 1) % testimonials.length);
+        setSlideDirection('slide-in');
+      }, 500); // Duration of the slide-out animation
+    }, 4000); // Change testimonial every 4 seconds
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    if (testimonialRef.current)
+      setContainerHeight(`${testimonialRef.current.scrollHeight}px`);
+  }, [currentTestimonial]);
+
   return (
     <div className={styles.splashpage}>
       <div className={styles.content}>
@@ -53,6 +87,17 @@ function Landing() {
           <Link to="/catalog" className={clsx(styles.btn, styles.guest)}>
             Guest
           </Link>
+        </div>
+        <div
+          className={styles.testimonialWrapper}
+          style={{ height: containerHeight }}
+        >
+          <p
+            ref={testimonialRef}
+            className={clsx(styles.testimonialText, styles[slideDirection])}
+          >
+            "{testimonials[currentTestimonial]}"
+          </p>
         </div>
       </div>
       <img
