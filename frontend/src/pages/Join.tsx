e6994b9842bbe90af1feffe83d02fba7a173081f
alex s <46074070+bearsyankees@users.noreply.github.com>
2024-08-26 01:46:59 -0400

    Update Join.tsx
    new message?

--- frontend/src/pages/Join.tsx
+++ frontend/src/pages/Join.tsx
@@ -8,16 +8,14 @@ function Join() {
       <h1 className={clsx(styles.joinHeader, 'mt-5 mb-3')}>Join Us!</h1>
       <p className={clsx(styles.joinDescription, 'mb-5')}>
         <TextComponent type="secondary">
-          Current applications are closed :/ But we will be recruiting again in
-          the spring!
+          Our application process will be opening soon for the fall! Stay tuned for updates.
         </TextComponent>
       </p>
       <p className={clsx(styles.joinDescription, 'mb-5')}>
         <TextComponent type="tertiary">
           CourseTable is open source, so feel free to contribute to our{' '}
-          <a href="https://github.com/coursetable/coursetable">GitHub</a>! And{' '}
-          <a href="mailto:coursetable.at.yale@gmail.com"> email us</a> if you
-          have any questions.
+          <a href="https://github.com/coursetable/coursetable">GitHub</a>! If you have any questions, don't hesitate to{' '}
+          <a href="mailto:coursetable.at.yale@gmail.com">email us</a>.
         </TextComponent>
       </p>
     </div>
