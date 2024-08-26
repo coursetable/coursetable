001972a08a55b5f4475ddb2f7122f3fb15a72adb
bearsyankees <bearsyankees@gmail.com>
2024-08-26 11:40:41 -0400

    pretty

--- frontend/src/pages/Join.tsx
+++ frontend/src/pages/Join.tsx
@@ -8,13 +8,15 @@ function Join() {
       <h1 className={clsx(styles.joinHeader, 'mt-5 mb-3')}>Join Us!</h1>
       <p className={clsx(styles.joinDescription, 'mb-5')}>
         <TextComponent type="secondary">
-          Our application process will be opening soon for the fall! Stay tuned for updates.
+          Our application process will be opening soon for the fall! Stay tuned
+          for updates.
         </TextComponent>
       </p>
       <p className={clsx(styles.joinDescription, 'mb-5')}>
         <TextComponent type="tertiary">
           CourseTable is open source, so feel free to contribute to our{' '}
-          <a href="https://github.com/coursetable/coursetable">GitHub</a>! If you have any questions, don't hesitate to{' '}
+          <a href="https://github.com/coursetable/coursetable">GitHub</a>! If
+          you have any questions, don't hesitate to{' '}
           <a href="mailto:coursetable.at.yale@gmail.com">email us</a>.
         </TextComponent>
       </p>
