fc16d0ad912c94cfa917bb9ec43c8690aa57f317
Neil Song <song.neil.song@gmail.com>
2024-10-07 22:58:48 -0400

    fix: lint

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 import { createAuthSlice, type AuthSlice } from './slices/AuthSlice';
 import {
   createDimensionsSlice,
