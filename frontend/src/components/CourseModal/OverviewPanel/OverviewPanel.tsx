ba869c81906190de685a7da7e30b6669d2b757f2
Joshua Chen <sidachen2003@gmail.com>
2024-11-01 18:09:45 -0400

    feat: better error handling for course modal data fetching

--- frontend/src/components/CourseModal/OverviewPanel/OverviewPanel.tsx
+++ frontend/src/components/CourseModal/OverviewPanel/OverviewPanel.tsx
@@ -1,4 +1,5 @@
 import { Row, Col } from 'react-bootstrap';
+import { MdWarning } from 'react-icons/md';
 
 import OverviewInfo from './OverviewInfo';
 import OverviewRatings from './OverviewRatings';
@@ -33,15 +34,62 @@ function OverviewPanel({
   });
 
   // Wait until data is fetched
-  if (loading || error || !data) return <Spinner />;
+  if (loading) return <Spinner />;
+
+  if (error) {
+    return (
+      <Row className="m-auto">
+        <Col>
+          <p>Failed to load data: {error.message}</p>
+        </Col>
+      </Row>
+    );
+  }
+
+  const { sameCourse = [], sameProf = [], self: [listing] = [] } = data ?? {};
+
+  if (!listing) {
+    return (
+      <Row className="m-auto">
+        <Col>
+          <p>Unexpected: no data returned.</p>
+          <p>
+            There could be some issues with our servers. Please allow for up to
+            10 minutes for course information to become available again.
+          </p>
+        </Col>
+      </Row>
+    );
+  }
+
+  // TODO: properly fix this data
+  const isSameCourseWrong = !sameCourse.some((c) =>
+    c.listings.some((l) => l.crn === listing.crn),
+  );
 
   return (
     <Row className="m-auto">
       <Col md={7} className="px-0 mt-0 mb-3">
-        <OverviewInfo onNavigation={onNavigation} data={data} />
+        <OverviewInfo
+          onNavigation={onNavigation}
+          listing={listing}
+          sameCourse={sameCourse}
+        />
       </Col>
       <Col md={5} className="px-0 my-0">
-        <OverviewRatings onNavigation={onNavigation} data={data} />
+        {isSameCourseWrong && (
+          <div className="alert alert-warning">
+            <MdWarning className="mr-2" />
+            <strong>Warning:</strong> We have detected a possible error in the
+            data returned. Try opening CourseTable in a new tab.
+          </div>
+        )}
+        <OverviewRatings
+          onNavigation={onNavigation}
+          listing={listing}
+          sameCourse={sameCourse}
+          sameProf={sameProf}
+        />
       </Col>
     </Row>
   );
