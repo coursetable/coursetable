416b15473c732f96da8655d3ce514580ece8422b
Joshua Chen <sidachen2003@gmail.com>
2024-11-14 13:37:28 -0500

    fix: stop tracking USER_NOT_FOUND errors

--- frontend/src/queries/api.ts
+++ frontend/src/queries/api.ts
@@ -124,10 +124,22 @@ async function fetchAPI(
       } catch {}
       // Fall back to status text
       errorCode ||= res.statusText;
-      // Let the handler handle it first
-      if (handleErrorCode?.(errorCode))
-        return noResExpected ? false : undefined;
-      throw new Error(errorCode);
+      // Handle common errors uniformly
+      switch (errorCode) {
+        case 'USER_NOT_FOUND':
+          toast.info('Login expired. Please log in again.');
+          return noResExpected ? false : undefined;
+        case 'INVALID_REQUEST':
+          toast.error(
+            'The server did not understand this request. Please refresh the page and try again.',
+          );
+          return noResExpected ? false : undefined;
+        default:
+          // Let the handler handle it first
+          if (handleErrorCode?.(errorCode))
+            return noResExpected ? false : undefined;
+          throw new Error(errorCode);
+      }
     }
     // If no res body is expected, return early
     if (noResExpected) return true;
@@ -142,12 +154,9 @@ async function fetchAPI(
       message: body ? `${breadcrumb.message} ${payload}` : breadcrumb.message,
     });
     Sentry.captureException(err);
-    let message = String(err);
-    if (message.includes('INVALID_REQUEST')) {
-      message +=
-        ' Please try refreshing the page and/or reopening in a new tab.';
-    }
-    toast.error(`Failed while ${breadcrumb.message.toLowerCase()}: ${message}`);
+    toast.error(
+      `Failed while ${breadcrumb.message.toLowerCase()}: ${String(err)}`,
+    );
     return noResExpected ? false : undefined;
   }
 }
