537846a8c23ce64ad58885af580536c3d4cca90a
Abheek Dhawan <abheekd@protonmail.com>
2024-11-25 19:19:54 -0600

    fix: make image all-in-one instead of specific size

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -136,8 +136,8 @@ export default defineConfig({
         icons: [
           {
             src: 'icon200x200.png',
-            sizes: '200x200',
-            type: 'image/png',
+            // sizes: '200x200',
+            // type: 'image/png',
           },
         ],
         display: 'standalone',
