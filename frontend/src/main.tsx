256d73bf58329be2005cc8bd445efe95d8ce9135
Abheek Dhawan <abheekd@protonmail.com>
2024-12-13 17:14:31 -0500

    fix: revert extra newline

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,7 +12,6 @@ import * as Sentry from '@sentry/react';
 // Globals has to be imported first, because it contains all the base CSS!
 // eslint-disable-next-line import/order
 import Globals from './Globals';
-
 import App from './App';
 import { isDev } from './config';
 import { TutorialProvider } from './contexts/tutorialContext';
