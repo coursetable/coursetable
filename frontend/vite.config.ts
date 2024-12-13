7f17f0e46fb2d37dc8eccbe5e86923d72999df18
Abheek Dhawan <abheekd@protonmail.com>
2024-12-13 16:12:13 -0500

    fix: update based on review comments

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -136,8 +136,6 @@ export default defineConfig({
         icons: [
           {
             src: 'icon200x200.png',
-            // Sizes: '200x200',
-            // type: 'image/png',
           },
         ],
         display: 'standalone',
