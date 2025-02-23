b4ecd16436d712e479f096e9f78c803ac38b7dec
Humphrey Xu <29512438+Etherite1@users.noreply.github.com>
2025-02-23 15:34:43 -0500

    fix

--- frontend/src/components/Worksheet/WorksheetCalendarListItem.tsx
+++ frontend/src/components/Worksheet/WorksheetCalendarListItem.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import { ListGroup } from 'react-bootstrap';
+import { useShallow } from 'zustand/react/shallow';
 import WorksheetHideButton from './WorksheetHideButton';
 import WorksheetToggleButton from './WorksheetToggleButton';
 import type { CatalogListing } from '../../queries/api';
@@ -15,6 +16,12 @@ export default function WorksheetCalendarListItem({
   readonly listing: CatalogListing;
   readonly hidden: boolean;
 }) {
+  const { viewedPerson } = useStore(
+    useShallow((state) => ({
+      viewedPerson: state.viewedPerson,
+    })),
+  );
+
   const target = useCourseModalLink(listing);
   const setHoverCourse = useStore((state) => state.setHoverCourse);
 
@@ -37,14 +44,16 @@ export default function WorksheetCalendarListItem({
         <span className={styles.courseTitle}>{listing.course.title}</span>
       </Link>
       <div className="d-flex align-items-center gap-1">
-        <WorksheetHideButton
-          crn={listing.crn}
-          hidden={hidden}
-          className={clsx(
-            styles.hideButton,
-            !hidden && styles.hideButtonHidden,
-          )}
-        />
+        {viewedPerson === 'me' && (
+          <WorksheetHideButton
+            crn={listing.crn}
+            hidden={hidden}
+            className={clsx(
+              styles.hideButton,
+              !hidden && styles.hideButtonHidden,
+            )}
+          />
+        )}
         <WorksheetToggleButton listing={listing} modal={false} />
       </div>
     </ListGroup.Item>
