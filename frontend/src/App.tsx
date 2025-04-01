cd563abcecb005e6d698ea188166f24c77eabba1
Neil Song <song.neil.song@gmail.com>
2025-03-31 22:32:39 -0400

    banner message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -143,7 +143,16 @@ function App() {
         // Don't remove this wrapper.
         id={16}
       >
-        # Placeholder
+        Got tea on Yale Hospitality? Take a 2 minute{' '}
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSc6asiy6REcJUADw-F9BQq-GnFHbyLOdbIULH5OVgTSRZQpPw/viewform"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: 'white', textDecoration: 'underline' }}
+        >
+          Spill the Tea Survey
+        </a>{' '}
+        and be entered to win a one of 10 ($20) local coffee shop gift cards.
       </Notice>
       <Navbar />
       <SentryRoutes>
