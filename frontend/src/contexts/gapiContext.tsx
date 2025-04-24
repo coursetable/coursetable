79f735f2d16ae940e329144b7978ad013249b173
Neil Song <song.neil.song@gmail.com>
2025-04-23 21:43:33 -0400

    fix: migrate to gis from gapi.auth2

--- frontend/src/contexts/gapiContext.tsx
+++ frontend/src/contexts/gapiContext.tsx
@@ -5,12 +5,10 @@ import React, {
   useMemo,
   useState,
 } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 type Store = {
   gapi: typeof globalThis.gapi | null;
-  authInstance: gapi.auth2.GoogleAuthBase | null;
-  user: gapi.auth2.GoogleUser | null;
-  setUser: React.Dispatch<React.SetStateAction<gapi.auth2.GoogleUser | null>>;
 };
 
 const GapiContext = createContext<Store | undefined>(undefined);
@@ -25,40 +23,35 @@ export function GapiProvider({
   readonly children: React.ReactNode;
 }) {
   const [gapi, setGapi] = useState<typeof globalThis.gapi | null>(null);
-  const [authInstance, setAuthInstance] =
-    useState<gapi.auth2.GoogleAuthBase | null>(null);
-  const [user, setUser] = useState<gapi.auth2.GoogleUser | null>(null);
 
   // Load gapi script and client
   useEffect(() => {
     async function loadGapi() {
-      const { loadGapiInsideDOM, loadAuth2 } = await import('gapi-script');
+      const { loadGapiInsideDOM } = await import('gapi-script');
       const newGapi = await loadGapiInsideDOM();
       await new Promise((resolve) => {
         newGapi.load(GAPI_CLIENT_NAME, resolve);
       });
-      const [, , newAuth2] = await Promise.all([
+      await Promise.all([
         newGapi.client.init({
           apiKey: import.meta.env.VITE_DEV_GCAL_API_KEY,
           clientId: import.meta.env.VITE_DEV_GCAL_CLIENT_ID,
           scope: SCOPES,
         }),
         newGapi.client.load('calendar', 'v3'),
-        loadAuth2(newGapi, import.meta.env.VITE_DEV_GCAL_CLIENT_ID, SCOPES),
       ]);
       setGapi(newGapi);
-      setAuthInstance(newAuth2);
-      if (newAuth2.isSignedIn.get()) setUser(newAuth2.currentUser.get());
     }
     void loadGapi();
   }, []);
 
-  const store: Store = useMemo(
-    () => ({ gapi, authInstance, user, setUser }),
-    [gapi, authInstance, user],
-  );
+  const store: Store = useMemo(() => ({ gapi }), [gapi]);
 
-  return <GapiContext.Provider value={store}>{children}</GapiContext.Provider>;
+  return (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_DEV_GCAL_CLIENT_ID}>
+      <GapiContext.Provider value={store}>{children}</GapiContext.Provider>
+    </GoogleOAuthProvider>
+  );
 }
 
 export const useGapi = () => useContext(GapiContext)!;
