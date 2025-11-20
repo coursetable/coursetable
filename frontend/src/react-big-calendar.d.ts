0c56282dc3e08a617125248036c3c370428b8ccf
aryans-15 <aryan.sharma0714@gmail.com>
2025-11-19 20:29:24 -0500

    misc: add todo comment to react-big-calendar augmentation

--- frontend/src/react-big-calendar.d.ts
+++ frontend/src/react-big-calendar.d.ts
@@ -1,5 +1,6 @@
 import 'react-big-calendar';
 
+// TODO: remove augmentation once @types/react-big-calendar includes showCurrentTimeIndicator
 declare module 'react-big-calendar' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface CalendarProps<_TEvent = object, _TResource = object> {
