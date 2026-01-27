40f67e4501f701500ae2b63b2e48b5529ef04cfa
WFeng7 <wsfeng714@gmail.com>
2026-01-26 21:59:59 -0500

    graphql queries for cross listed friends

---
+++ api/src/friends/friends.queries.ts
@@ -0,0 +1,119 @@
+import * as Types from '../graphql-types.js';
+
+import { GraphQLClient, RequestOptions } from 'graphql-request';
+import _gql from 'graphql-tag';
+
+const gql = _gql as unknown as typeof import('graphql-tag').default;
+type GraphQLClientRequestHeaders = RequestOptions['requestHeaders'];
+export type CrnToSameCourseIdQueryVariables = Types.Exact<{
+  crns: Array<Types.Scalars['Int']['input']> | Types.Scalars['Int']['input'];
+  season: Types.Scalars['String']['input'];
+}>;
+
+export type CrnToSameCourseIdQuery = {
+  __typename?: 'query_root';
+  listings: Array<{
+    __typename?: 'listings';
+    crn: number;
+    course: { __typename?: 'courses'; sameCourseId: number };
+  }>;
+};
+
+export type AllCrnsForSameCourseIdsQueryVariables = Types.Exact<{
+  sameCourseIds:
+    | Array<Types.Scalars['Int']['input']>
+    | Types.Scalars['Int']['input'];
+  season: Types.Scalars['String']['input'];
+}>;
+
+export type AllCrnsForSameCourseIdsQuery = {
+  __typename?: 'query_root';
+  courses: Array<{
+    __typename?: 'courses';
+    sameCourseId: number;
+    listings: Array<{ __typename?: 'listings'; crn: number }>;
+  }>;
+};
+
+export const CrnToSameCourseIdDocument = gql`
+  query CrnToSameCourseId($crns: [Int!]!, $season: String!) {
+    listings(
+      where: { crn: { _in: $crns }, course: { season_code: { _eq: $season } } }
+    ) {
+      crn
+      course {
+        sameCourseId: same_course_id
+      }
+    }
+  }
+`;
+export const AllCrnsForSameCourseIdsDocument = gql`
+  query AllCrnsForSameCourseIds($sameCourseIds: [Int!]!, $season: String!) {
+    courses(
+      where: {
+        same_course_id: { _in: $sameCourseIds }
+        season_code: { _eq: $season }
+      }
+    ) {
+      sameCourseId: same_course_id
+      listings {
+        crn
+      }
+    }
+  }
+`;
+
+export type SdkFunctionWrapper = <T>(
+  action: (requestHeaders?: Record<string, string>) => Promise<T>,
+  operationName: string,
+  operationType?: string,
+  variables?: any,
+) => Promise<T>;
+
+const defaultWrapper: SdkFunctionWrapper = (
+  action,
+  _operationName,
+  _operationType,
+  _variables,
+) => action();
+
+export function getSdk(
+  client: GraphQLClient,
+  withWrapper: SdkFunctionWrapper = defaultWrapper,
+) {
+  return {
+    CrnToSameCourseId(
+      variables: CrnToSameCourseIdQueryVariables,
+      requestHeaders?: GraphQLClientRequestHeaders,
+    ): Promise<CrnToSameCourseIdQuery> {
+      return withWrapper(
+        (wrappedRequestHeaders) =>
+          client.request<CrnToSameCourseIdQuery>(
+            CrnToSameCourseIdDocument,
+            variables,
+            { ...requestHeaders, ...wrappedRequestHeaders },
+          ),
+        'CrnToSameCourseId',
+        'query',
+        variables,
+      );
+    },
+    AllCrnsForSameCourseIds(
+      variables: AllCrnsForSameCourseIdsQueryVariables,
+      requestHeaders?: GraphQLClientRequestHeaders,
+    ): Promise<AllCrnsForSameCourseIdsQuery> {
+      return withWrapper(
+        (wrappedRequestHeaders) =>
+          client.request<AllCrnsForSameCourseIdsQuery>(
+            AllCrnsForSameCourseIdsDocument,
+            variables,
+            { ...requestHeaders, ...wrappedRequestHeaders },
+          ),
+        'AllCrnsForSameCourseIds',
+        'query',
+        variables,
+      );
+    },
+  };
+}
+export type Sdk = ReturnType<typeof getSdk>;
